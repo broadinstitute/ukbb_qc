c8801044a7ef8cb73d101e090effc232e923f04f
Katherine Chao <kchao@broadinstitute.org>
2019-03-05 17:52:05 -0500

    Added path to adj-filtered, split multi mt for tranche 1

---
+++ resources.py
@@ -0,0 +1,4 @@
+
+regeneron_tranche_1_mt = 'gs://broad-ukbb/data/regeneron.freeze_4.nf.mt'
+regeneron_tranche_1_adj_split_multi_ht = 'gs://broad-ukbb/data/regeneron.freeze_4.nf_adj.mt'
+ukbb_array_dir = 'gs://broad-ukbb/data/array/'
