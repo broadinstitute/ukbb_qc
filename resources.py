5cffec4ae9ee5aba819270530f8490781642491d
gtiao <gtiao@broadinstitute.org>
2019-03-28 14:59:41 -0400

    Cleaning up filepaths and flow

--- resources.py
+++ resources.py
@@ -1,3 +1,25 @@
+# Resources for UKBB QC
 
-regeneron_tranche_1_mt = 'gs://broad-ukbb/data/regeneron.freeze_4.nf.mt'
 ukbb_array_dir = 'gs://broad-ukbb/data/array/'
+ukbb_calling_intervals = 'gs://broad-ukbb/data/white_album_exome_calling_regions.v1.interval_list'
+ukbb_calling_intervals_summary = 'gs://broad-ukbb/data/ukbb_exome_calling_intervals.summary.txt'
+gc_content_bed = 'gs://broad-ukbb/data/gc5Base.bed'
+
+
+def intervals_ht(data_source: str, freeze: int = CURRENT_FREEZE) -> str:
+    return f'{sample_qc_prefix(data_source, freeze)}/coverage/intervals.ht'
+
+
+def coverage_by_target(data_source: str, freeze: int = CURRENT_FREEZE, ht: bool = True) -> str:
+    prefix = f'{sample_qc_prefix(data_source, freeze)}/coverage/coverage_by_target'
+    if ht:
+        return prefix + '.ht'
+    else:
+        return prefix + '.txt'
+
+def coverage_by_region_type(data_source: str, freeze: int = CURRENT_FREEZE, gc_bin_type: str, ht: bool = True) -> str:
+    prefix = f'{sample_qc_prefix(data_source, freeze)}/coverage/coverage_by_region_type.{gc_bin_type}'
+    if ht:
+        return prefix + '.ht'
+    else:
+        return prefix + '.txt'
