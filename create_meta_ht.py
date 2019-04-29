1221960e31ea5cefb2752689d3e2b0e58785cba1
Katherine Chao <kchao@broadinstitute.org>
2019-04-29 09:56:15 -0400

    Added Grace's requested changes to meta ht creation script

--- create_meta_ht.py
+++ create_meta_ht.py
@@ -8,7 +8,7 @@ logger = logging.getLogger("create_meta_ht")
 logger.setLevel(logging.INFO)
 
 
-def sample_comparison(ht1, ht2) -> bool:
+def compare_samples(ht1, ht2) -> bool:
     """
     Checks if two sample counts are the same
 
@@ -25,7 +25,7 @@ def sample_comparison(ht1, ht2) -> bool:
     return s_count1 == s_count2
 
 
-def table_join(
+def join_tables(
     left_ht: hl.Table, left_key: str, right_ht: hl.Table, right_key: str, join_type: str
     ) -> hl.Table:
     """
@@ -41,12 +41,12 @@ def table_join(
     """
 
 
-    sample_count_match = sample_comparison(left_ht, right_ht)
+    sample_count_match = compare_samples(left_ht, right_ht)
 
     if not sample_count_match:
         logger.warning('Sample counts in left and right tables do not match')
 
-    return left_ht.key_by(left_key).join(right_ht.key_by(right_key), how = join_type)
+    return left_ht.key_by(left_key).join(right_ht.key_by(right_key), how=join_type)
 
 
 def main(args):
@@ -64,50 +64,49 @@ def main(args):
     right_ht = hl.read_table(qc_ht_path(data_source, freeze))
 
     logger.info('Joining hard filters ht and qc ht to start making meta ht')
-    left_ht = table_join(left_ht, 's', right_ht, 's', 'outer')
+    left_ht = join_tables(left_ht, 's', right_ht, 's', 'outer')
 
     logger.info('Reading in platform PCA ht')
     right_ht = hl.read_table(platform_pca_results_ht_path(data_source, freeze))
-    right_ht = right_ht.annotate(platform_pca_scores = right_ht.scores)
-    right_ht = right_ht.drop('scores')
+    right_ht = right_ht.transmute(platform_pca_scores=right_ht.scores)
 
     logger.info('Joining platform PCA ht into current join (meta ht)')
-    left_ht = table_join(left_ht, 's', right_ht, 's', 'outer')
+    left_ht = join_tables(left_ht, 's', right_ht, 's', 'outer')
 
     logger.info('Reading in population PC ht')
     right_ht = hl.read_table(ancestry_pc_project_scores_ht_path(data_source, freeze, None))
 
     logger.info('Joining population PC ht into current join')
-    left_ht = table_join(left_ht, 's', right_ht, 's', 'outer')
+    left_ht = join_tables(left_ht, 's', right_ht, 's', 'outer')
 
     logger.info('Creating checkpoint')
-    left_ht = left_ht.checkpoint(get_ht_checkpoint_path(data_source, freeze, 'intermediate_ht_join'), overwrite = True)
+    left_ht = left_ht.checkpoint(get_ht_checkpoint_path(data_source, freeze, 'intermediate_ht_join'), overwrite=True)
 
     logger.info('Reading in array sample concordance ht')
     right_ht = hl.read_table(array_sample_concordance_path(data_source, freeze))
 
     logger.info('Joining array sample concordance ht to current join')
-    left_ht = left_ht.annotate(array_id = left_ht.s.split("_")[1])
-    left_ht = table_join(left_ht, 'array_id', right_ht, 's', 'left')
+    left_ht = left_ht.annotate(array_id=left_ht.s.split("_")[1])
+    left_ht = join_tables(left_ht, 'array_id', right_ht, 's', 'left')
 
     logger.info('Reading in dups ht')
     right_ht = hl.read_table(duplicates_ht_path(data_source, freeze, args.dup_sets))
     right_ht = right_ht.drop('qc_mt_params')
 
     logger.info('Joining dups ht to current join')
-    left_ht = table_join(left_ht, 's', right_ht, 's', 'left')
+    left_ht = join_tables(left_ht, 's', right_ht, 's', 'left')
 
     logger.info('Reading in related samples ht')
     related_samples_to_drop_ht = hl.read_table(related_drop_path(data_source, freeze))
 
     logger.info('Annotating meta ht with related samples')
-    left_ht = left_ht.annotate(related_filter = hl.is_defined(related_samples_to_drop_ht[left_ht.s]))
+    left_ht = left_ht.annotate(related_filter=hl.is_defined(related_samples_to_drop_ht[left_ht.s]))
    
     logger.info('Reading in outlier ht')
     right_ht = hl.read_table(platform_pop_outlier_ht_path(data_source, freeze)) 
 
     logger.info('Joining outlier ht to current join')
-    left_ht = table_join(left_ht, 's', right_ht, 's', 'left')
+    left_ht = join_tables(left_ht, 's', right_ht, 's', 'left')
 
     logger.info('Writing out meta ht')
     left_ht.write(meta_ht_path(data_source, freeze), overwrite = True)
