e8737c01047e01179306f9fae7e4d5bca054a160
Katherine Chao <kchao@broadinstitute.org>
2019-06-10 15:51:00 -0400

    Updated hard filters removal function in utils to work on mt and ht

--- utils.py
+++ utils.py
@@ -1,15 +1,16 @@
 from ukbb_qc.resources import *
 
 
-def remove_hard_filter_samples(data_source: str, freeze: int, mt: hl.MatrixTable) -> hl.MatrixTable:
+def remove_hard_filter_samples(data_source: str, freeze: int, t: Union[hl.MatrixTable, hl.Table]) -> Union[hl.MatrixTable, hl.Table]:
     """
-    Removes samples that failed hard filters from mt
+    Removes samples that failed hard filters from MatrixTable or Table. 
+    Assumes MatrixTable's col key/Table's row key is sample.
 
     :param str data_source: 'regeneron' or 'broad'
     :param int freeze: One of the data freezes
-    :param MatrixTable mt: Input MatrixTable
-    :return: MatrixTable with samples removed
-    :rtype: MatrixTable
+    :param MatrixTable/able t: Input MatrixTable or Table
+    :return: MatrixTable or Table with samples removed
+    :rtype: MatrixTable or Table
     """
 
     # read in hard filters ht
@@ -19,5 +20,8 @@ def remove_hard_filter_samples(data_source: str, freeze: int, mt: hl.MatrixTable
     ht = ht.select('hard_filters')
 
     # remove hard filtered samples
-    mt = mt.filter_cols(hl.len(ht[mt.col_key].hard_filters) == 0)
-    return mt
+    if isinstance(t, hl.MatrixTable):
+        t = t.filter_cols(hl.len(ht[t.col_key].hard_filters) == 0) 
+    else:
+        t.filter((hl.len(ht[t.row_key].hard_filters) == 0)
+    return t
