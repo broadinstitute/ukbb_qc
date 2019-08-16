1731d4c386a266999ef82f42da12e78736e825c7
Katherine Chao <kchao@broadinstitute.org>
2019-08-16 10:16:49 -0400

    Updated utils.py to print number of hard filtered samples and to remove variants unique to those samples

--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
+import logging
 from ukbb_qc.resources import *
+
+
+logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
+logger = logging.getLogger("utils")
+logger.setLevel(logging.INFO)
+
 
 
 def remove_hard_filter_samples(data_source: str, freeze: int, t: Union[hl.MatrixTable, hl.Table]) -> Union[hl.MatrixTable, hl.Table]:
@@ -19,9 +26,16 @@ def remove_hard_filter_samples(data_source: str, freeze: int, t: Union[hl.Matrix
     # remove unnecessary fields from hard filters ht
     ht = ht.select('hard_filters')
 
+    # get number of hard filtered samples
+    hf_samples = ht.aggregate(hl.agg.count_where(hl.len(ht.hard_filters) > 0))
+    logger.info(f'Removing {hf_samples} samples that failed hard filters')
+
     # remove hard filtered samples
+    ht = ht.filter(hl.len(ht.hard_filters) == 0)
     if isinstance(t, hl.MatrixTable):
-        t = t.filter_cols(hl.len(ht[t.col_key].hard_filters) == 0) 
+        t = t.filter_cols(hl.is_defined(ht[t.col_key]))
+        t = t.annotate_rows(non_refs=hl.agg.count_where(t.GT.is_non_ref()))
+        t = t.filter_rows(t.non_refs > 0).drop('non_refs')
     else:
         t.filter((hl.len(ht[t.row_key].hard_filters) == 0))
     return t
