cac7b8d59d7698ae7f194eeffb9337f121e6eccb
Katherine Chao <kchao@broadinstitute.org>
2019-09-11 14:09:43 -0400

    Updated paths (removed hardcoded paths) from assess_variant_qc_results

--- assess_variant_qc_results.py
+++ assess_variant_qc_results.py
@@ -91,26 +91,27 @@ def main(args):
         joint_ht.export(f'{variant_qc_prefix(data_source, freeze)}/assessment/joint_gnomad_broad_regeneron.{run_hash}.agg.txt.bgz')
 
 
-    ### PICK UP HERE ###
+    ### PICK UP HERE ### 
+    # NOTE: I updated the paths in the code, but the tables are all still in temp 
     if args.aggregate_nfe:
 
+        # Read in gnomAD and filter to NFE, identify variants only in NFE samples
         if args.filter_nfe:
-            # Read in gnomAD and filter to NFE, identify those variants
+            # NOTE: I (KC) used this table (gs://broad-ukbb/broad.freeze_4/temp/gnomad_nfe.ht); I didn't rerun this block of code from 101-107
             gnomad_mt = get_gnomad_data('exomes')
             rows, cols = gnomad_mt.count()
             gnomad_mt = gnomad_mt.filter_cols(gnomad_mt.meta.pop == 'nfe', keep=True)
             logger.info(f'Found {gnomad_mt.count_cols()} NFE samples out of {cols} original samples in gnomAD exomes')
             gnomad_mt = gnomad_mt.filter_rows(hl.agg.sum(gnomad_mt.GT.n_alt_alleles()) > 0, keep=True)
             logger.info(f'Found {gnomad_mt.count_rows()} variants in NFE samples out of {rows} original variants in all gnomAD exomes')
-            gnomad_mt.rows().write('gs://broad-ukbb/broad.freeze_4/temp/gnomad_nfe.ht', overwrite=args.overwrite)
+            gnomad_mt.rows().write(f'{variant_qc_prefix(data_source, freeze)}/assessment/gnomad_nfe.ht', overwrite=args.overwrite)
 
-            # Read in UKBB and filter to NFE, identify those variants
-            # this reads in split hardcalls (non-adj)
+            # Read in UKBB split hardcalls (non-adj)  and filter to NFE, identify those variants
             ukbb_mt = get_ukbb_data(data_source, freeze)
             rows, cols = ukbb_mt.count()
 
-            # Keep only NFE found with gnomAD PC project
-            # hard code path until we replace meta ht with pop adj meta ht
+            # Keep only NFE samples identified with gnomAD PC project
+            # NOTE: hard code path until we replace Regeneron meta ht with pop adj meta ht
             meta_ht = hl.read_table(f'gs://broad-ukbb/regeneron.freeze_{freeze}/sample_qc/meta_w_pop_adj.ht')
             meta_ht = meta_ht.annotate(is_nfe=(meta_ht.gnomad_pc_project_pop == 'nfe'))
             logger.info(f'NFE samples in Regeneron meta table: {meta_ht.aggregate(hl.agg.counter(meta_ht.is_nfe))}')
@@ -121,31 +122,31 @@ def main(args):
             ukbb_mt = ukbb_mt.annotate_rows(nfe_ac=hl.agg.sum(ukbb_mt.GT.n_alt_alleles()))
             ukbb_mt = ukbb_mt.filter_rows(ukbb_mt.nfe_ac > 0, keep=True)
             logger.info(f'Found {ukbb_mt.count_rows()} variants in NFE samples out of {rows} original variants in all UKBB exomes')
-            ukbb_mt.rows().write(f'gs://broad-ukbb/broad.freeze_{freeze}/temp/ukbb_nfe.ht', overwrite=args.overwrite)
+            ukbb_mt.rows().write(f'{variant_qc_prefix(data_source, freeze)}/assessment/ukbb_nfe.ht', overwrite=args.overwrite)
 
         # Filter joint ht to those variants
-        gnomad_ht = hl.read_table(f'gs://broad-ukbb/broad.freeze_{freeze}/temp/gnomad_nfe.ht').select()
+        gnomad_ht = hl.read_table(f'{variant_qc_prefix(data_source, freeze)}/assessment/gnomad_nfe.ht').select()
         rows = gnomad_ht.count()
         liftover_ht = hl.read_table(
-            'gs://gnomad-public/release/2.1.1/liftover_grch38/ht/exomes/gnomad.exomes.r2.1.1.sites.liftover_grch38.ht').key_by('original_locus', 'original_alleles').rename({'locus' : 'locus_grch38', 'alleles' : 'alleles_grch38'})
+            get_gnomad_liftover_data_path('exomes', version=2.1.1)).key_by('original_locus', 'original_alleles').rename(
+                {'locus' : 'locus_grch38', 'alleles' : 'alleles_grch38'})
         gnomad_ht = gnomad_ht.annotate(**liftover_ht[gnomad_ht.key])
         gnomad_ht = gnomad_ht.filter(hl.is_defined(gnomad_ht.locus_grch38))
         logger.info(
             f'Found {gnomad_ht.count()} variants in lifted-over NFE samples out of {rows} original variants in NFE gnomAD exomes')
         gnomad_ht = gnomad_ht.key_by('locus_grch38', 'alleles_grch38')
 
-        ukbb_ht = hl.read_table(f'gs://broad-ukbb/broad.freeze_{freeze}/temp/ukbb_nfe.ht').select()
-        nfe_ht = ukbb_ht.join(gnomad_ht, how='outer').checkpoint(f'gs://broad-ukbb/broad.freeze_{freeze}/temp/joint_gnomad_ukbb.nfe.ht',
+        ukbb_ht = hl.read_table(f'{variant_qc_prefix(data_source, freeze)}/assessment/ukbb_nfe.ht').select()
+        nfe_ht = ukbb_ht.join(gnomad_ht, how='outer').checkpoint(f'{variant_qc_prefix(data_source, freeze)}/assessment/joint_gnomad_ukbb.nfe.ht',
                                                                 overwrite=args.overwrite)
 
-        #joint_ht = hl.read_table(f'gs://broad-ukbb/broad.freeze_{freeze}/temp/joint_gnomad_ukbb.ht')
         joint_ht = hl.read_table(f'{variant_qc_prefix(data_source, freeze)}/assessment/joint_gnomad_broad_regeneron.{run_hash}.ht')
-        nfe_ht = hl.read_table(f'gs://broad-ukbb/broad.freeze_{freeze}/temp/joint_gnomad_ukbb.nfe.ht')
+        nfe_ht = hl.read_table(f'{variant_qc_prefix(data_source, freeze)}/assessment/joint_gnomad_ukbb.nfe.ht')
 
         joint_ht = joint_ht.filter(hl.is_defined(nfe_ht[joint_ht.key]))
         logger.info(f'Found {joint_ht.count()} variants in NFE samples in both gnomAD and UKBB')
 
-        # # Note: freq[2] contains the NFE variant allele frequencies in gnomAD
+        # NOTE: freq[2] contains the NFE variant allele frequencies in gnomAD
         joint_ht = joint_ht.filter((joint_ht.freq[2].AF <= 0.05) & (joint_ht.freq[2].AF > 0.00001), keep=True)
         joint_ht = joint_ht.annotate(gnomad_af_bin=hl.case()
                                     .when((joint_ht.freq[0].AF <= 0.00001), "(0, 0.00001]")
@@ -165,22 +166,21 @@ def main(args):
                             n=hl.agg.count()))
         joint_ht = joint_ht.annotate(frac_shared=joint_ht.n_shared/joint_ht.n,
                                         frac_gnomad_unique=joint_ht.n_gnomad_unique/joint_ht.n)
-        joint_ht = joint_ht.checkpoint(f'gs://broad-ukbb/broad.freeze_{freeze}/temp/joint_gnomad_ukbb.nfe.agg.ht', overwrite=args.overwrite)
+        joint_ht = joint_ht.checkpoint(f'{variant_qc_prefix(data_source, freeze)}/assessment/joint_gnomad_ukbb.nfe.agg.ht', overwrite=args.overwrite)
         joint_ht.show(50)
-        joint_ht.export(f'gs://broad-ukbb/broad.freeze_{freeze}/temp/joint_gnomad_ukbb.nfe.agg.tsv.gz')
+        joint_ht.export(f'{variant_qc_prefix(data_source, freeze)}/assessment/joint_gnomad_ukbb.nfe.agg.tsv.gz')
 
 
     if args.bin_rf_probs_nfe:
-        #joint_ht = hl.read_table(f'gs://broad-ukbb/broad.freeze_{freeze}/temp/joint_gnomad_ukbb.nfe.ht')
         joint_ht = hl.read_table(f'{variant_qc_prefix(data_source, freeze)}/assessment/joint_gnomad_broad_regeneron.{run_hash}.ht')
         joint_ht = joint_ht.group_by(tp_bin = joint_ht.rf_probability // 0.01).aggregate(
                             n_pass_gnomad=hl.agg.count_where(hl.is_defined(joint_ht.pass_gnomad) & joint_ht.pass_gnomad),
                             n_fail_gnomad=hl.agg.count_where(hl.is_defined(joint_ht.pass_gnomad) & ~joint_ht.pass_gnomad),
                             n_unique_ukbb=hl.agg.count_where(~hl.is_defined(joint_ht.pass_gnomad)),
                             n=hl.agg.count())
-        joint_ht = joint_ht.checkpoint('gs://broad-ukbb/broad.freeze_4/temp/joint_gnomad_ukbb.nfe.probs.ht', overwrite=args.overwrite)
+        joint_ht = joint_ht.checkpoint(f'{variant_qc_prefix(data_source, freeze)}/assessment/joint_gnomad_ukbb.nfe.probs.ht', overwrite=args.overwrite)
         joint_ht.show(50)
-        joint_ht.export('gs://broad-ukbb/broad.freeze_4/temp/joint_gnomad_ukbb.nfe.probs.tsv.gz')
+        joint_ht.export(f'gs://broad-ukbb/broad.freeze_{freeze}/assessment/joint_gnomad_ukbb.nfe.probs.tsv.gz')
 
 
     if args.eval_sib_singletons:
