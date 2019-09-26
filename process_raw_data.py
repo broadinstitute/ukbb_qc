deb4a5bc248968cc410a7d972dc02294155d256e
jkgoodrich <jgoodric@broadinstitute.org>
2019-09-26 10:42:25 -0400

    Some modifications to parameters

--- process_raw_data.py
+++ process_raw_data.py
@@ -32,12 +32,14 @@ def main(args):
     data_source = args.data_source
     freeze = args.freeze
 
-    if not args.skip_compute_qc_mt:
+    if args.compute_qc_mt:
         logger.info("Filtering to bi-allelic, high-callrate, common SNPs for sample QC...")
         mt = get_ukbb_data(data_source, freeze, raw=True, adj=True, split=False)
 
         lcr = get_lcr_intervals()
-        mt = mt.filter_rows(hl.is_missing(lcr[mt.row_key]))
+        logger.info(f'Total number of variants in raw unsplit matrix table: {mt.count()[0]}')
+        mt = mt.filter_rows(hl.is_missing(lcr[mt.locus]))
+        logger.info(f'Total number of variants after LCR filtering: {mt.count()[0]}')
 
         if data_source == 'regeneron':
             apply_hard_filters = False
@@ -46,22 +48,30 @@ def main(args):
 
         # Todo: changed to use Laurent's code for the qc_mt, change is that it uses inbreeding_coeff and hardy_weinberg_threshold, is this OK and are defaults OK?
         # Todo: add segdup and decoy filter
-        qc_mt = get_qc_mt(mt, min_af=0.001, min_callrate=0.99, apply_hard_filters=apply_hard_filters, ld_r2=None,
-                          filter_lcr=False, filter_decoy=False, filter_segdup=False)
+        qc_mt = get_qc_mt(mt,
+                          min_af=arg.min_af,
+                          min_callrate=args.min_callrate,
+                          apply_hard_filters=apply_hard_filters,
+                          ld_r2=None,
+                          filter_lcr=False,
+                          filter_decoy=False,
+                          filter_segdup=False)
         qc_mt = qc_mt.naive_coalesce(5000)
         qc_mt.write(qc_mt_path(data_source, freeze), overwrite=args.overwrite)
+        logger.info(f'Total number of variants in bi-allelic, high-callrate, common SNPs for sample QC: {qc_mt.count()[0]}')
 
+    if args.compute_sample_qc_ht:
         qc_mt = hl.read_matrix_table(qc_mt_path(data_source, freeze))
         qc_mt = filter_to_autosomes(qc_mt)
         qc_ht = hl.sample_qc(qc_mt).cols().select('sample_qc')
         qc_ht = qc_ht.transmute(sample_qc=qc_ht.sample_qc.select('call_rate', 'gq_stats', 'dp_stats'))
         qc_ht.write(qc_ht_path(data_source, freeze), overwrite=args.overwrite)
 
-    if not args.skip_ld_prune_qc_mt:
+    if args.ld_prune_qc_mt:
         logger.info("LD prune qc MatrixTable sample QC...")
         qc_mt = hl.read_matrix_table(qc_mt_path(data_source, freeze))
         qc_mt = qc_mt.unfilter_entries()
-        pruned_qc_mt = ld_prune_qc_mt(qc_mt)
+        pruned_qc_mt = ld_prune_qc_mt(qc_mt, ld_r2=args.ld_r2)
         variants, samples = pruned_qc_mt.count()
         logger.info(f'{variants} variants found in LD-pruned MT')
         pruned_qc_mt.write(qc_mt_path(data_source, freeze, ld_pruned=True), overwrite=args.overwrite)
@@ -74,8 +84,21 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--data_source', help='Data source', choices=['regeneron', 'broad'], default='broad')
     parser.add_argument('-f', '--freeze', help='Data freeze to use', default=CURRENT_FREEZE, type=int)
 
-    parser.add_argument('--skip_compute_qc_mt', help='Skip Compute matrix to be used in sample qc', action='store_true')
-    parser.add_argument('--skip_ld_prune_qc_mt', help='Skip LD prunes the qc matrix', action='store_true')
+    parser.add_argument('--compute_qc_mt', help='Compute matrix to be used in sample qc', action='store_true')
+    parser.add_argument('--min_callrate',
+                        help='Minimum variant callrate to retain variant in qc matrix table.',
+                        default=0.99,
+                        type=float)
+    parser.add_argument('--min_af',
+                        help='Minimum variant allele frequency to retain variant in qc matrix table.',
+                        default=0.001,
+                        type=float)
+    parser.add_argument('--compute_sample_qc_ht', help='Compute sample qc on qc matrix table', action='store_true')
+    parser.add_argument('--ld_prune_qc_mt', help='LD prunes the qc matrix', action='store_true')
+    parser.add_argument('--ld_r2',
+                        help='r2 to LD prune the QC matrix to.',
+                        default=0.1,
+                        type=float)
 
     args = parser.parse_args()
 
