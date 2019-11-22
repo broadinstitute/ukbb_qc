7482b8cefa87f679084768cef682292b871906ea
Katherine Chao <kchao@broadinstitute.org>
2019-11-22 10:04:07 -0500

    Updated densify sparse mt code to test repartition before densify and repartition on read

--- densify_sparse_mt.py
+++ densify_sparse_mt.py
@@ -9,16 +9,37 @@ logger = logging.getLogger('densify_sparse_mt')
 logger.setLevel(logging.INFO)
 
 
+def rep_on_read(path, n_desired):
+     mt = hl.read_matrix_table(path)
+     intervals = mt._calculate_new_partitions(n_desired)
+     return hl.read_matrix_table(path, _intervals=intervals)
+
+
 def main(args):
 
     hl.init(default_reference='GRCh38', log='/densify_sparse_mt.log')    
     freeze = args.freeze
+    n = args.n_partitions
+    out_path = f'gs://broad-ukbb/broad.freeze_{freeze}/temp/' 
 
     logger.info('Reading in input mt (raw sparse mt)')
-    #mt = get_ukbb_data('broad', freeze, adj=False, split=False, raw=True)
     mt = hl.read_matrix_table(args.input)
     logger.info(f'Sparse mt count: {mt.count()}')
 
+    # subset of 10 samples
+    samples = ['UKB_1000026_233144425', 'UKB_1000031_236163960', 'UKB_1000083_232997991', 'UKB_1000122_232973530', 'UKB_1000139_232977596', 'UKB_1000218_232941722', 'UKB_1000276_236163521', 'UKB_1000447_232921119', 'UKB_1000486_233000970', 'UKB_1000494_236259219']
+
+    logger.info('Filtering to chr20 and sample subset')
+    mt = hl.filter_intervals(mt, [hl.parse_locus_interval('chr20')])
+    mt = mt.filter_cols(hl.literal(samples).contains(mt.s))
+    logger.info(f'Count after filtration: {mt.count()}')
+
+    if args.before_densify:
+        out_path = out_path + 'dense_subset.mt'
+        mt = mt.repartition(n)
+    else:
+        out_path = out_path + 'temp_dense_subset.mt'
+        
     logger.info('Densifying mt')
     mt = hl.experimental.densify(mt)
 
@@ -27,15 +48,29 @@ def main(args):
     logger.info(f'Count after filtration: {mt.count()}')
 
     logger.info('Writing out mt')
-    mt = mt.repartition(30000)
-    mt.write(raw_mt_path('broad', freeze), args.overwrite)
+    mt.write(out_path, args.overwrite)
+
+    if args.on_read:
+        logger.info('Reading in densified sparse mt and repartitioning')
+        mt = rep_on_read(out_path, n)
+
+        logger.info('Filtering to calling intervals')
+        capture_ht = hl.read_table(capture_ht_path('broad', freeze))
+        mt = mt.filter_rows(hl.is_defined(capture_ht[mt.locus])) 
+        logger.info(f'Count after filtration: {mt.count()}')
+
+        logger.info('Writing out mt')
+        out_path = out_path.replace('temp_', '')
+        mt.write(out_path, args.overwrite)
    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', help='Path to input mt', required=True)
     parser.add_argument('-f', '--freeze', help='Data freeze to use', default=CURRENT_FREEZE, type=int)
-    parser.add_argument('--test', help='Filter to chr20 to test', action='store_true')
+    parser.add_argument('-n', '--n_partitions', help='Number of desired partitions', type=int)
+    parser.add_argument('--before_densify', help='Repartition before writing', action='store_true')
+    parser.add_argument('--on_read', help='Repartition on read', action='store_true')
     parser.add_argument('--overwrite', help='Overwrite data', action='store_true')
     parser.add_argument('--slack_channel', help='Slack channel to post results and notifications to.')
     args = parser.parse_args()
