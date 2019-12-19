99aae1752be228f8519a30fb52291f4c21c4d27f
jkgoodrich <jgoodric@broadinstitute.org>
2019-12-19 13:52:06 -0700

    Fixed relatedness path

--- relatedness_inference.py
+++ relatedness_inference.py
@@ -144,7 +144,7 @@ def main(args):
             dup_kin_cutoff=args.dup_kinship_cutoff
         )
 
-        related_samples_to_drop_ht = related_samples_to_drop_ht.checkpoint(f'{sample_qc_prefix(data_source, freeze)}/relatedness/related_samples_to_drop.pc_relate.new2.ht', overwrite=args.overwrite)
+        related_samples_to_drop_ht = related_samples_to_drop_ht.checkpoint(related_drop_path(data_source, freeze), overwrite=args.overwrite)
         logger.info(f'{related_samples_to_drop_ht.filter(related_samples_to_drop_ht.relationship == "second-degree").count()} second degree samples flagged in callset using maximal independent set')
         logger.info(f'{related_samples_to_drop_ht.filter(related_samples_to_drop_ht.relationship == "first-degree").count()} first degree samples flagged in callset using maximal independent set')
         logger.info(f'{related_samples_to_drop_ht.filter(related_samples_to_drop_ht.relationship == "duplicate").count()} duplicate samples flagged in callset using maximal independent set')
