b6b49340a6fb02f26234f6636a624226078acde5
jkgoodrich <jgoodric@broadinstitute.org>
2020-01-22 12:24:02 -0500

    Modified interval qc and filter to accommodate sex chromosome logic. Changed interval QC to treat undefined GT as 0 DP. Small formatting changes.

--- interval_qc.py
+++ interval_qc.py
@@ -3,89 +3,152 @@ import ukbb_qc.resources as urc
 from gnomad_hail import try_slack, filter_to_autosomes
 import argparse
 import logging
+from typing import *
 
 logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
 logger = logging.getLogger("interval_qc")
 logger.setLevel(logging.INFO)
 
 
-def main(args):
-    hl.init(log="/interval_qc.log")
+def interval_qc(
+    mt: hl.MatrixTable,
+    interval_ht: hl.Table,
+    split_by_sex: bool = False,
+    checkpoint_path: Optional[str] = None,
+):
+    """
+    Determines the percent of samples reaching 10x, 15x, 20x, 25x, and 30x mean coverage in each interval
 
-    data_source = args.data_source
-    freeze = args.freeze
+    :param MatrixTable mt: Matrix table to use for interval QC
+    :param Table interval_ht: Table containing the intervals that interval QC is performed over
+    :param bool split_by_sex: Should the interval QC be stratified by sex, if True, mt must be annotated with sex_karyotype
+    :param str checkpoint_path: Optional path to a file to checkpoint the mean_dp per sample across each interval MT
+    :return: Table with percent samples with coverage at 10x, 15x, 20x, 25x, and 30x coverage
+    :rtype: Table
+    """
 
-    mt = urc.get_ukbb_data(data_source, freeze, raw=True, adj=False, split=False)
-    mt = mt.key_rows_by("locus", "alleles")
-    mt = mt.select_entries(mt.DP)
-
-    if args.chromosome is not None:
-        mt = hl.filter_intervals(
-            mt,
-            [
-                hl.parse_locus_interval(
-                    f"chr{args.chromosome}", reference_genome="GRCh38"
-                )
-            ],
-        )
-    else:
-        mt = filter_to_autosomes(mt)
-
-    ht = hl.read_table(urc.capture_ht_path(data_source, freeze))
-    ht = ht.annotate(interval_str=hl.str(ht.interval))
-    mt = mt.annotate_rows(**ht[mt.locus])
-    ht = ht.key_by("interval_str")
-    mt = mt.checkpoint(
-        urc.get_mt_checkpoint_path(
-            data_source, freeze, "DP_only_checkpoint_interval_qc"
-        ),
-        overwrite=args.overwrite,
+    interval_ht = interval_ht.annotate(
+        interval_label=interval_ht.interval
     )
 
+    mt = mt.annotate_rows(interval=interval_ht[mt.locus].interval_label)
+
     target_mt = (
-        mt.group_rows_by("interval_str")
-        .partition_hint(5000)
+        mt.group_rows_by(mt.interval)
         .aggregate_entries(
-            mean_dp=hl.agg.filter(
-                (hl.is_defined(mt.DP)) & (~hl.is_nan(mt.DP)), hl.agg.mean(mt.DP)
-            ),
-            pct_gt_20x=hl.agg.filter(
-                hl.is_defined(mt.DP), hl.agg.fraction(mt.DP >= 20)
-            ),
+            mean_dp=hl.agg.mean(hl.cond(hl.is_defined(mt.DP), mt.DP, 0)),
+            pct_gt_20x=hl.agg.fraction(hl.cond(hl.is_defined(mt.DP), mt.DP, 0) >= 20),
             pct_dp_defined=hl.agg.count_where(hl.is_defined(mt.DP)) / hl.agg.count(),
         )
         .result()
     )
     target_mt = target_mt.naive_coalesce(5000)
-    target_mt = target_mt.checkpoint(
-        urc.get_mt_checkpoint_path(data_source, freeze, "coverage_by_target_samples"),
-        overwrite=args.overwrite,
-    )
-    target_mt = target_mt.annotate_rows(
-        target_num_mean_dp_defined=hl.agg.count_where(~hl.is_nan(target_mt.mean_dp)),
-        target_mean_dp=hl.agg.filter(
-            ~hl.is_nan(target_mt.mean_dp), hl.agg.mean(target_mt.mean_dp)
-        ),
-        target_pct_gt_20x=hl.agg.mean(target_mt.pct_gt_20x),
-        target_mean_pct_dp_defined=hl.agg.mean(target_mt.pct_dp_defined),
-        pct_samples_15x=hl.agg.fraction(target_mt.mean_dp >= 15),
-        pct_samples_20x=hl.agg.fraction(target_mt.mean_dp >= 20),
-        pct_samples_25x=hl.agg.fraction(target_mt.mean_dp >= 25),
-        pct_samples_30x=hl.agg.fraction(target_mt.mean_dp >= 30),
-    )
+    target_mt = target_mt.checkpoint(checkpoint_path, overwrite=True)
+    if split_by_sex:
+        target_mt = target_mt.annotate_rows(
+            target_mean_dp=hl.agg.group_by(
+                target_mt.sex_karyotype,
+                hl.agg.filter(
+                    ~hl.is_nan(target_mt.mean_dp), hl.agg.mean(target_mt.mean_dp)
+                ),
+            ),
+            target_pct_gt_20x=hl.agg.group_by(
+                target_mt.sex_karyotype, hl.agg.mean(target_mt.pct_gt_20x)
+            ),
+            pct_samples_10x=hl.agg.group_by(
+                target_mt.sex_karyotype, hl.agg.fraction(target_mt.mean_dp >= 10)
+            ),
+            pct_samples_15x=hl.agg.group_by(
+                target_mt.sex_karyotype, hl.agg.fraction(target_mt.mean_dp >= 15)
+            ),
+            pct_samples_20x=hl.agg.group_by(
+                target_mt.sex_karyotype, hl.agg.fraction(target_mt.mean_dp >= 20)
+            ),
+            pct_samples_25x=hl.agg.group_by(
+                target_mt.sex_karyotype, hl.agg.fraction(target_mt.mean_dp >= 25)
+            ),
+            pct_samples_30x=hl.agg.group_by(
+                target_mt.sex_karyotype, hl.agg.fraction(target_mt.mean_dp >= 30)
+            ),
+        )
+    else:
+        target_mt = target_mt.annotate_rows(
+            target_mean_dp=hl.agg.filter(
+                ~hl.is_nan(target_mt.mean_dp), hl.agg.mean(target_mt.mean_dp)
+            ),
+            target_pct_gt_20x=hl.agg.mean(target_mt.pct_gt_20x),
+            pct_samples_10x=hl.agg.fraction(target_mt.mean_dp >= 10),
+            pct_samples_15x=hl.agg.fraction(target_mt.mean_dp >= 15),
+            pct_samples_20x=hl.agg.fraction(target_mt.mean_dp >= 20),
+            pct_samples_25x=hl.agg.fraction(target_mt.mean_dp >= 25),
+            pct_samples_30x=hl.agg.fraction(target_mt.mean_dp >= 30),
+        )
 
     target_ht = target_mt.rows()
-    target_ht = target_ht.annotate(**ht[target_ht.interval_str])
-    target_ht = target_ht.key_by("interval")
     target_ht = target_ht.naive_coalesce(100)
-
-    target_ht.write(
-        urc.interval_qc_path(data_source, freeze, chrom=args.chromosome),
-        overwrite=args.overwrite,
-    )
-    target_ht = hl.read_table(urc.interval_qc_path(data_source, freeze))
-
     target_ht.describe()
+
+    return target_ht
+
+
+def main(args):
+    hl.init(log="/interval_qc.log")
+
+    if not args.autosomes and not args.sex_chr:
+        logger.warning("Must choose one of autosomes or sex_chr options")
+
+    data_source = args.data_source
+    freeze = args.freeze
+    ht = hl.read_table(urc.capture_ht_path(data_source, freeze))
+
+    checkpoint_path = urc.get_mt_checkpoint_path(
+        data_source, freeze, "DP_only_checkpoint_interval_qc_broad_tranche1" #"DP_only_checkpoint_interval_qc"
+    )
+    if not hl.utils.hadoop_exists(f"{checkpoint_path}/_SUCCESS"):
+        mt = urc.get_ukbb_data(data_source, freeze, raw=True, adj=False, split=False)
+        mt = mt.key_rows_by("locus", "alleles")
+        mt = mt.select_entries(mt.DP)
+        mt.write(checkpoint_path, overwrite=True)
+
+    if args.autosomes:
+        mt = hl.read_matrix_table(checkpoint_path)
+        mt = filter_to_autosomes(mt)
+        target_ht = interval_qc(
+            mt,
+            ht,
+            checkpoint_path=urc.get_mt_checkpoint_path(
+                data_source, freeze, "coverage_by_target_samples"
+            ),
+        )
+        target_ht.write(
+            urc.interval_qc_path(data_source, freeze, "autosomes"),
+            overwrite=args.overwrite,
+        )
+    if args.sex_chr:
+        mt = hl.read_matrix_table(checkpoint_path)
+        mt = hl.filter_intervals(
+            mt,
+            [
+                hl.parse_locus_interval(f"chrX", reference_genome="GRCh38"),
+                hl.parse_locus_interval(f"chrY", reference_genome="GRCh38")
+            ]
+        )
+        sex_ht = hl.read_table(urc.sex_ht_path(data_source, freeze))
+        mt = mt.annotate_cols(**sex_ht[mt.col_key])
+        mt.describe()
+        mt = mt.filter_cols((mt.sex_karyotype == "XX") | (mt.sex_karyotype == "XY"))
+        target_ht = interval_qc(
+            mt,
+            ht,
+            split_by_sex=True,
+            checkpoint_path=urc.get_mt_checkpoint_path(
+                data_source, freeze, "coverage_by_target_samples"
+            ),
+        )
+        target_ht.write(
+            urc.interval_qc_path(data_source, freeze, "sex_chr"),
+            overwrite=args.overwrite,
+        )
 
 
 if __name__ == "__main__":
@@ -107,15 +170,23 @@ if __name__ == "__main__":
         default="broad",
     )
     parser.add_argument(
-        "-f", "--freeze", help="Data freeze to use", default=urc.CURRENT_FREEZE, type=int
+        "-f",
+        "--freeze",
+        help="Data freeze to use",
+        default=urc.CURRENT_FREEZE,
+        type=int,
     )
 
     parser.add_argument(
-        "-c",
-        "--chromosome",
-        type=int,
-        choices=range(1, 23),
-        help="If set it will only run the given chromosome",
+        "--autosomes",
+        action="store_true",
+        help="If set it will only run the autosomes",
+    )
+
+    parser.add_argument(
+        "--sex_chr",
+        action="store_true",
+        help="If set it will only run the sex chromosomes",
     )
 
     args = parser.parse_args()
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import logging
 from ukbb_qc.resources import *
+import hail as hl
 
 
 logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
@@ -7,7 +8,9 @@ logger = logging.getLogger("utils")
 logger.setLevel(logging.INFO)
 
 
-def remove_hard_filter_samples(data_source: str, freeze: int, t: Union[hl.MatrixTable, hl.Table]) -> Union[hl.MatrixTable, hl.Table]:
+def remove_hard_filter_samples(
+    data_source: str, freeze: int, t: Union[hl.MatrixTable, hl.Table]
+) -> Union[hl.MatrixTable, hl.Table]:
     """
     Removes samples that failed hard filters from MatrixTable or Table. 
     Assumes MatrixTable's col key/Table's row key is sample.
@@ -23,69 +26,127 @@ def remove_hard_filter_samples(data_source: str, freeze: int, t: Union[hl.Matrix
     ht = hl.read_table(hard_filters_ht_path(data_source, freeze))
 
     # remove unnecessary fields from hard filters ht
-    ht = ht.select('hard_filters')
+    ht = ht.select("hard_filters")
 
     # get number of hard filtered samples
     hf_samples = ht.aggregate(hl.agg.count_where(hl.len(ht.hard_filters) > 0))
-    logger.info(f'Removing {hf_samples} samples that failed hard filters')
+    logger.info(f"Removing {hf_samples} samples that failed hard filters")
 
     # remove hard filtered samples
     ht = ht.filter(hl.len(ht.hard_filters) == 0)
     if isinstance(t, hl.MatrixTable):
         t = t.filter_cols(hl.is_defined(ht[t.col_key]))
         t = t.annotate_rows(non_refs=hl.agg.count_where(t.GT.is_non_ref()))
-        t = t.filter_rows(t.non_refs > 0).drop('non_refs')
+        t = t.filter_rows(t.non_refs > 0).drop("non_refs")
     else:
         t.filter((hl.len(ht[t.key].hard_filters) == 0))
     return t
 
 
-def interval_qc_filter(data_source: str,
-                       freeze: int,
-                       t: Union[hl.MatrixTable, hl.Table],
-                       pct_samples_20x: float = 0.85) -> Union[hl.MatrixTable, hl.Table]:
+def annotate_interval_qc_filter(
+    data_source: str,
+    freeze: int,
+    t: Union[hl.MatrixTable, hl.Table],
+    cov_filter_field: str = "pct_samples_20x",
+    XY_cov_filter_field: str = "pct_samples_10x",
+    pct_samples: float = 0.85,
+    autosomes_only: bool = False
+) -> Union[hl.MatrixTable, hl.Table]:
     """
     Removes poorly covered intervals from a MatrixTable or Table.
 
     :param str data_source: 'regeneron' or 'broad'
     :param int freeze: One of the data freezes
     :param MatrixTable/able t: Input MatrixTable or Table
-    :param float pct_samples_20x: Percent of samples with coverage greater than 20X over the interval for filtering
+    :param float cov_filter_field: Coverage field to use for filtering of autosomes/sex chr par/female X
+    :param float XY_cov_filter_field: Coverage field to use for filtering of male X and Y
+    :param float pct_samples: Percent of samples with coverage greater than `cov_filter_field` over the interval for filtering
     :return: MatrixTable or Table with samples removed
     :rtype: MatrixTable or Table
     """
-    interval_qc_ht = hl.read_table(interval_qc_path(data_source, freeze))
-    good_intervals_ht = interval_qc_ht.filter(interval_qc_ht.pct_samples_20x > pct_samples_20x).key_by('interval')
+    interval_qc_ht = hl.read_table(interval_qc_path(data_source, freeze, "autosomes"))
+    good_intervals_ht = interval_qc_ht.filter(
+        interval_qc_ht[cov_filter_field] > pct_samples
+    )
+
+    if not autosomes_only:
+        interval_qc_sex_ht = hl.read_table(
+            interval_qc_path(data_source, freeze, "sex_chr")
+        )
+
+        good_sex_intervals_ht = interval_qc_sex_ht.filter(
+            (
+                interval_qc_sex_ht.interval.start.in_autosome_or_par()
+                & (interval_qc_sex_ht[cov_filter_field]["XX"] > pct_samples)
+                & (interval_qc_sex_ht[cov_filter_field]["XY"] > pct_samples)
+            )
+            | (
+                (interval_qc_sex_ht.interval.start.contig == "chrX")
+                & (interval_qc_sex_ht[cov_filter_field]["XX"] > pct_samples)
+                & (interval_qc_sex_ht[XY_cov_filter_field]["XY"] > pct_samples)
+            )
+            | (
+                (interval_qc_sex_ht.interval.start.contig == "chrY")
+                & (interval_qc_sex_ht[XY_cov_filter_field]["XY"] > pct_samples)
+            )
+        )
+        good_intervals_ht = good_intervals_ht.select().union(good_sex_intervals_ht.select())
 
     if isinstance(t, hl.MatrixTable):
-        t = t.filter_rows(hl.is_defined(good_intervals_ht[t.locus]))
+        t = t.annotate_rows(
+            interval_qc_pass=hl.is_defined(good_intervals_ht[t.locus])
+        )
     else:
-        t = t.filter(hl.is_defined(good_intervals_ht[t.locus]))
+        t = t.annotate(
+            interval_qc_pass=hl.is_defined(good_intervals_ht[t.locus])
+        )
+
     return t
 
 
-def annotate_relationship(relatedness_ht,
-                          first_degree_threshold=[0.1767767, 0.4],
-                          second_degree_threshold=0.08838835,
-                          ibd2_parent_offspring_threshold=0.14):
-    relatedness_ht = relatedness_ht.annotate(relationship_classification=hl.case()
-                                             .when((relatedness_ht.kin > second_degree_threshold) &
-                                                   (relatedness_ht.kin < first_degree_threshold[0]), 'Second-degree')
-                                             .when((relatedness_ht.kin > first_degree_threshold[0]) &
-                                                   (relatedness_ht.kin < first_degree_threshold[1]) &
-                                                   (relatedness_ht.ibd2 >= ibd2_parent_offspring_threshold),
-                                                   'Full-sibling')
-                                             .when((relatedness_ht.kin > first_degree_threshold[0]) &
-                                                   (relatedness_ht.kin < first_degree_threshold[1]) &
-                                                   (relatedness_ht.ibd2 < ibd2_parent_offspring_threshold),
-                                                   'Parent-child')
-                                             .default("None"))
+def annotate_relationship(
+    relatedness_ht: hl.Table,
+    first_degree_threshold: List[float] = [0.1767767, 0.4],
+    second_degree_threshold: float = 0.08838835,
+    ibd2_parent_offspring_threshold: float = 0.14
+):
+    """
+    Annotates a kinship Table with second-degree, full-sibling, and parent-child relationships
+
+    :param Table relatedness_ht: kinship Table to be annotated
+    :param List of floats first_degree_threshold: lower and upper kinship thresholds for first degree relatedness
+    :param float second_degree_threshold: kinship threshold for second degree relatedness
+    :param float ibd2_parent_offspring_threshold: IBD2 threshold to differentiate parent-child from full-sibling
+    :return: None
+    :rtype: None
+    """
+    relatedness_ht = relatedness_ht.annotate(
+        relationship_classification=hl.case()
+        .when(
+            (relatedness_ht.kin > second_degree_threshold)
+            & (relatedness_ht.kin < first_degree_threshold[0]),
+            "Second-degree",
+        )
+        .when(
+            (relatedness_ht.kin > first_degree_threshold[0])
+            & (relatedness_ht.kin < first_degree_threshold[1])
+            & (relatedness_ht.ibd2 >= ibd2_parent_offspring_threshold),
+            "Full-sibling",
+        )
+        .when(
+            (relatedness_ht.kin > first_degree_threshold[0])
+            & (relatedness_ht.kin < first_degree_threshold[1])
+            & (relatedness_ht.ibd2 < ibd2_parent_offspring_threshold),
+            "Parent-child",
+        )
+        .default("None")
+    )
 
     return relatedness_ht
 
 
 def import_capture_regions(interval_path: str, output_path: str, ow: bool) -> None:
-    '''
+    """
     Imports capture region text file into Table and writes Table at specified path
 
     :param str interval_path: Path to input file
@@ -93,25 +154,23 @@ def import_capture_regions(interval_path: str, output_path: str, ow: bool) -> No
     :param bool ow: Whether to overwrite data
     :return: None
     :rtype: None
-    '''
+    """
 
-    logger.info('Importing capture table')
+    logger.info("Importing capture table")
     capture_ht = hl.import_table(
-                    interval_path,
-                    no_header=True,
-                    impute=True,
-                    min_partitions=10)
+        interval_path, no_header=True, impute=True, min_partitions=10
+    )
 
     # seqnames	start	end	width	strand	target_type	region_type	target_id
-    #chr1	11719	12377	659	*	processed_transcript|transcribed_unprocessed_pseudogene|ice_target_1	gnomad	target_1
-    capture_ht = capture_ht.transmute(interval=hl.parse_locus_interval(
-                                    hl.format('[%s:%s-%s]',
-                                               capture_ht.f0,
-                                               capture_ht.f1,
-                                               capture_ht.f2),
-                                    reference_genome='GRCh38'))
-    capture_ht = capture_ht.select('interval').key_by('interval')
+    # chr1	11719	12377	659	*	processed_transcript|transcribed_unprocessed_pseudogene|ice_target_1	gnomad	target_1
+    capture_ht = capture_ht.transmute(
+        interval=hl.parse_locus_interval(
+            hl.format("[%s:%s-%s]", capture_ht.f0, capture_ht.f1, capture_ht.f2),
+            reference_genome="GRCh38",
+        )
+    )
+    capture_ht = capture_ht.select("interval").key_by("interval")
 
     capture_ht.describe()
-    logger.info('Writing capture ht')
+    logger.info("Writing capture ht")
     capture_ht.write(output_path, overwrite=ow)
