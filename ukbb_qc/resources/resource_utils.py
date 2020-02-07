37d247879f10bee24dbaf1daece80140793992b1
Katherine Chao <kchao@broadinstitute.org>
2020-02-07 14:58:17 -0500

    Moved CURRENT_FREEZE and other useful variables to resource_utils

--- ukbb_qc/resources/resource_utils.py
+++ ukbb_qc/resources/resource_utils.py
@@ -3,3 +3,9 @@ import hail as hl
 import logging
 from typing import Any, Dict, List, Optional, Tuple, Union
 from gnomad_hail.resources.resource_utils import DataException
+
+
+CURRENT_FREEZE = 5
+DATA_SOURCES = ['regeneron', 'broad']
+FREEZES = [4, 5]
+CURRENT_HAIL_VERSION = "0.2"
