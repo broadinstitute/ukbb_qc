5f83fb071f80431734d0bba5664b71d62bec54a0
Katherine Chao <kchao@broadinstitute.org>
2020-02-18 12:52:56 -0500

    Updated array sample map table loading to flag samples with withdrawn consents

--- ukbb_qc/load_data/utils.py
+++ ukbb_qc/load_data/utils.py
@@ -1,6 +1,7 @@
 import hail as hl
 import logging
-from ukbb_qc.resources.basics import array_sample_map
+from gnomad_hail.utils.generic import file_exists
+from ukbb_qc.resources.basics import array_sample_map, excluded_samples_path
 from ukbb_qc.resources.resource_utils import CURRENT_FREEZE
 
 
@@ -28,6 +29,18 @@ def import_array_exome_id_map_ht(freeze: int = CURRENT_FREEZE): -> hl.Table
     logger.info(
             f"Total number of IDs in the array to exome sample map: {sample_map_ht.count()}..."
         )
+
+    if file_exists(excluded_samples_path(freeze)):
+        excluded_samples_ht = hl.import_table(excluded_samples_path(freeze), no_header=True)
+        excluded_samples = hl.literal(ht.aggregate(hl.agg.collect_as_set(ht.f0)))
+        logger.info(
+            f"Total number of samples to exclude: {hl.eval(hl.len(excluded_samples))}"
+            )
+        sample_map_ht = sample_map_ht.annotate(withdrawn_consent=excluded_samples.contains(sample_map_ht.eid_26041))
+        withdrawn_ids = sample_map_ht.aggregate(hl.agg.count_where(sample_map_ht.withdrawn_consent))
+        logger.info(
+            f"Total number of IDs with withdrawn consents in sample map ht: {withdrawn_ids}"
+            )
     return sample_map_ht
 
 
