d01687239dd62cb3c75de79cbf1d40ff59d309dd
Katherine Chao <kchao@broadinstitute.org>
2020-02-20 13:06:31 -0500

    Added code to calculate callrate and mean DP on sparse mt to utils

--- ukbb_qc/utils/utils.py
+++ ukbb_qc/utils/utils.py
@@ -1,7 +1,10 @@
 import hail as hl
 import logging
 from typing import Union
-from gnomad_hail.utils.generic import filter_to_autosomes, get_reference_genome
+from gnomad_hail.utils.generic import (
+    filter_to_autosomes, interval_length, 
+    get_reference_genome, union_intervals
+    )
 from gnomad_hail.resources.grch38.intervals import lcr
 from ukbb_qc.resources.basics import get_ukbb_data, raw_mt_path
 from ukbb_qc.resources.sample_qc import f_stat_sites_path, interval_qc_path, qc_temp_data_prefix, qc_sites_path
@@ -108,6 +111,112 @@ def annotate_interval_qc_filter(
         )
 
     return t
+
+
+def get_sparse_sample_qc(mt: hl.MatrixTable) -> hl.Table:
+    """
+    Calculates approximation for call rate and mean DP on a sparse MatrixTable. 
+
+    :param MatrixTable mt: Input sparse MatrixTable
+    :return: Table of samples with their call rate and mean DP
+    :rtype: hl.Table
+    """
+    # Read in interval QC table and get total number of bases in high coverage intervals
+    interval_qc_ht = hl.read_table(interval_qc_path(data_source, freeze, "autosomes"))
+    good_intervals_ht = interval_qc_ht.filter(
+        interval_qc_ht[cov_filter_field] > pct_samples
+    )
+    good_intervals_ht = good_intervals_ht.annotate(interval_label=good_intervals_ht.interval)
+    good_intervals = good_intervals_ht.aggregate(hl.agg.collect(good_intervals_ht.interval))
+    total_bases = 0
+    for i in good_intervals:
+        total_bases += interval_length(i)
+    logger.info(f"Total number of bases in high coverage intervals: {total_bases}")
+
+
+    # Add END entry to rows
+    mt = mt.annotate_rows(
+        end=hl.or_missing(
+            hl.len(mt.alleles) == 1,
+            hl.agg.max(mt.END)
+        )
+    )
+
+    # Filter MatrixTable to high coverage intervals
+    logger.info(f"Total number of rows in input MatrixTable: {mt.count_rows()}")
+    mt = mt.annotate_rows(interval_qc_pass=(
+            hl.is_defined(good_intervals_ht[mt.row_key]) |
+            hl.is_defined(good_intervals_ht[hl.locus(mt.locus.contig, mt.end)])
+        )
+    )   
+    mt = mt.annotate_rows(interval=good_intervals_ht[mt.row_key].interval_label)
+    logger.info(f"Number of rows after filtering to high coverage intervals: {mt.count_rows()}")
+
+    # Add reference block start annotation to rows
+    # This is to only count bases in reference blocks that overlap high coverage intervals
+    mt = mt.annotate_rows(
+        start=hl.case()
+            .when(
+                hl.len(mt.alleles) == 1,
+                hl.if_else(
+                    mt.locus.position > mt.interval.start.position,
+                    mt.locus.position,
+                    mt.interval.start.position
+                )
+            )
+            .default(mt.locus.position)
+    )
+
+    # Add reference block interval annotation to entries
+    # Uses row annotation start for start of interval
+    # Adjusts end of interval to the minimum value between END entry and end of interval
+    mt = mt.annotate_entries(
+        ref_block_interval=(hl.or_missing(
+            mt.LGT.is_hom_ref(),
+            hl.parse_locus_interval(
+                hl.format(
+                    '%s:%s-%s', mt.locus.contig, mt.start, hl.if_else(
+                            mt.END < mt.interval.end.position,
+                            mt.END,
+                            mt.interval.end.position
+                        )
+                    )
+                )
+            )
+        )
+    )
+
+    # Add reference block length annotation to entries
+    mt = mt.annotate_entries(
+        ref_block_len=(hl.or_missing(
+            mt.LGT.is_hom_ref(),
+            mt.ref_block_interval.end.position - mt.ref_block_interval.start.position
+            )
+        )
+    )
+
+    # Use ref block length entry to calculate mean DP
+    mt = mt.annotate_cols(
+        mean_dp=hl.agg.sum(
+            hl.if_else(mt.LGT.is_hom_ref(), mt.DP * mt.ref_block_len, mt.DP)) 
+        / total_bases
+    )
+
+    # Use ref block length entry to create n_called column annotation
+    # n_called is the total number of bases in reference blocks and non ref sites for a sample
+    mt = mt.annotate_cols(
+        n_nonref=(hl.agg.count_where(mt.LGT.is_non_ref())),
+        total_ref_block_len=hl.agg.sum(mt.ref_block_len)
+    )
+    ht = mt.annotate_cols(
+        n_called=mt.n_nonref + mt.total_ref_block_len
+    ).cols()
+
+    # Calculate callrate and return 
+    ht = ht.annotate(
+        callrate=(ht.total_ref_block_len + ht.n_nonref) / total_bases
+    )
+    return ht
 
 
 # Sites resources
