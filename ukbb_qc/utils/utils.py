b180232e9c5b7abbe8f156f050c3c2130a5d0632
Katherine Chao <kchao@broadinstitute.org>
2020-02-24 16:19:54 -0500

    Reformatted all files in utils using black

--- ukbb_qc/utils/utils.py
+++ ukbb_qc/utils/utils.py
@@ -2,12 +2,19 @@ import hail as hl
 import logging
 from typing import Optional, Union
 from gnomad_hail.utils.generic import (
-    filter_to_autosomes, interval_length, 
-    get_reference_genome, union_intervals
-    )
+    filter_to_autosomes,
+    interval_length,
+    get_reference_genome,
+    union_intervals,
+)
 from gnomad_hail.resources.grch38.intervals import lcr
 from ukbb_qc.resources.basics import get_ukbb_data, raw_mt_path
-from ukbb_qc.resources.sample_qc import f_stat_sites_path, interval_qc_path, qc_temp_data_prefix, qc_sites_path
+from ukbb_qc.resources.sample_qc import (
+    f_stat_sites_path,
+    interval_qc_path,
+    qc_temp_data_prefix,
+    qc_sites_path,
+)
 
 
 logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
@@ -33,7 +40,7 @@ def compare_samples(ht1: hl.Table, ht2: hl.Table) -> bool:
 
 def join_tables(
     left_ht: hl.Table, left_key: str, right_ht: hl.Table, right_key: str, join_type: str
-    ) -> hl.Table:
+) -> hl.Table:
     """
     Joins two tables and returns joined table. Also prints warning if sample counts are not the same.
 
@@ -53,9 +60,11 @@ def join_tables(
 
 # Sample resources
 def remove_hard_filter_samples(
-    data_source: str, freeze: int, t: Union[hl.MatrixTable, hl.Table],
+    data_source: str,
+    freeze: int,
+    t: Union[hl.MatrixTable, hl.Table],
     gt_expr: Optional[hl.expr.CallExpression],
-    non_refs_only: bool = True
+    non_refs_only: bool = True,
 ) -> Union[hl.MatrixTable, hl.Table]:
     """
     Removes samples that failed hard filters from MatrixTable or Table. 
@@ -99,7 +108,7 @@ def annotate_interval_qc_filter(
     cov_filter_field: str = "pct_samples_20x",
     XY_cov_filter_field: str = "pct_samples_10x",
     pct_samples: float = 0.85,
-    autosomes_only: bool = False
+    autosomes_only: bool = False,
 ) -> Union[hl.MatrixTable, hl.Table]:
     """
     Removes poorly covered intervals from a MatrixTable or Table.
@@ -139,16 +148,14 @@ def annotate_interval_qc_filter(
                 & (interval_qc_sex_ht[XY_cov_filter_field]["XY"] > pct_samples)
             )
         )
-        good_intervals_ht = good_intervals_ht.select().union(good_sex_intervals_ht.select())
+        good_intervals_ht = good_intervals_ht.select().union(
+            good_sex_intervals_ht.select()
+        )
 
     if isinstance(t, hl.MatrixTable):
-        t = t.annotate_rows(
-            interval_qc_pass=hl.is_defined(good_intervals_ht[t.locus])
-        )
+        t = t.annotate_rows(interval_qc_pass=hl.is_defined(good_intervals_ht[t.locus]))
     else:
-        t = t.annotate(
-            interval_qc_pass=hl.is_defined(good_intervals_ht[t.locus])
-        )
+        t = t.annotate(interval_qc_pass=hl.is_defined(good_intervals_ht[t.locus]))
 
     return t
 
@@ -174,72 +181,85 @@ def get_sparse_sample_qc(mt: hl.MatrixTable, autosomes_only: bool = True) -> hl.
     good_intervals_ht = interval_qc_ht.filter(
         interval_qc_ht[cov_filter_field] > pct_samples
     )
-    good_intervals_ht = good_intervals_ht.annotate(interval_label=good_intervals_ht.interval)
-    good_intervals = good_intervals_ht.aggregate(hl.agg.collect(good_intervals_ht.interval))
+    good_intervals_ht = good_intervals_ht.annotate(
+        interval_label=good_intervals_ht.interval
+    )
+    good_intervals = good_intervals_ht.aggregate(
+        hl.agg.collect(good_intervals_ht.interval)
+    )
     total_bases = good_intervals_ht.aggregate(
         hl.agg.sum(
-            hl.abs(good_intervals_ht.interval.end.position - good_intervals_ht.interval.start.position)
+            hl.abs(
+                good_intervals_ht.interval.end.position
+                - good_intervals_ht.interval.start.position
+            )
         )
     )
     logger.info(f"Total number of bases in high coverage intervals: {total_bases}")
 
     # Add END entry to rows
     mt = mt.annotate_rows(
-        end=hl.or_missing(
-            hl.len(mt.alleles) == 1,
-            hl.agg.max(mt.END)
-        )
+        end=hl.or_missing(hl.len(mt.alleles) == 1, hl.agg.max(mt.END))
     )
 
     # Filter MatrixTable to high coverage intervals
     logger.info(f"Total number of rows in input MatrixTable: {mt.count_rows()}")
-    mt = mt.annotate_rows(interval_qc_pass=(
-            hl.is_defined(good_intervals_ht[mt.row_key]) |
-            hl.is_defined(good_intervals_ht[hl.locus(mt.locus.contig, mt.end)])
+    mt = mt.annotate_rows(
+        interval_qc_pass=(
+            hl.is_defined(good_intervals_ht[mt.row_key])
+            | hl.is_defined(good_intervals_ht[hl.locus(mt.locus.contig, mt.end)])
         )
-    )   
+    )
     mt = mt.annotate_rows(interval=good_intervals_ht[mt.row_key].interval_label)
-    logger.info(f"Number of rows after filtering to high coverage intervals: {mt.count_rows()}")
+    logger.info(
+        f"Number of rows after filtering to high coverage intervals: {mt.count_rows()}"
+    )
 
     # Add reference block start annotation to rows
     # This is to only count bases in reference blocks that overlap high coverage intervals
     mt = mt.annotate_rows(
         start=hl.case()
-            .when(
-                hl.len(mt.alleles) == 1,
-                hl.if_else(
-                    mt.locus.position > mt.interval.start.position,
-                    mt.locus.position,
-                    mt.interval.start.position
-                )
-            )
-            .default(mt.locus.position)
+        .when(
+            hl.len(mt.alleles) == 1,
+            hl.if_else(
+                mt.locus.position > mt.interval.start.position,
+                mt.locus.position,
+                mt.interval.start.position,
+            ),
+        )
+        .default(mt.locus.position)
     )
 
     # Add reference block interval annotation to entries
     # Uses row annotation start for start of interval
     # Adjusts end of interval to the minimum value between END entry and end of interval
     mt = mt.annotate_entries(
-        ref_block_interval=(hl.or_missing(
-            mt.LGT.is_hom_ref(),
-            hl.parse_locus_interval(
-                hl.format(
-                    '%s:%s-%s', mt.locus.contig, mt.start, hl.if_else(
+        ref_block_interval=(
+            hl.or_missing(
+                mt.LGT.is_hom_ref(),
+                hl.parse_locus_interval(
+                    hl.format(
+                        "%s:%s-%s",
+                        mt.locus.contig,
+                        mt.start,
+                        hl.if_else(
                             mt.END < mt.interval.end.position,
                             mt.END,
-                            mt.interval.end.position
-                        )
+                            mt.interval.end.position,
+                        ),
                     )
-                )
+                ),
             )
         )
     )
 
     # Add reference block length annotation to entries
     mt = mt.annotate_entries(
-        ref_block_len=(hl.or_missing(
-            mt.LGT.is_hom_ref(),
-            mt.ref_block_interval.end.position - mt.ref_block_interval.start.position
+        ref_block_len=(
+            hl.or_missing(
+                mt.LGT.is_hom_ref(),
+                mt.ref_block_interval.end.position
+                - mt.ref_block_interval.start.position,
             )
         )
     )
@@ -247,7 +267,8 @@ def get_sparse_sample_qc(mt: hl.MatrixTable, autosomes_only: bool = True) -> hl.
     # Use ref block length entry to calculate mean DP
     mt = mt.annotate_cols(
         mean_dp=hl.agg.sum(
-            hl.if_else(mt.LGT.is_hom_ref(), mt.DP * mt.ref_block_len, mt.DP)) 
+            hl.if_else(mt.LGT.is_hom_ref(), mt.DP * mt.ref_block_len, mt.DP)
+        )
         / total_bases
     )
 
@@ -255,24 +276,23 @@ def get_sparse_sample_qc(mt: hl.MatrixTable, autosomes_only: bool = True) -> hl.
     # n_called is the total number of bases in reference blocks and non ref sites for a sample
     mt = mt.annotate_cols(
         n_nonref=(hl.agg.count_where(mt.LGT.is_non_ref())),
-        total_ref_block_len=hl.agg.sum(mt.ref_block_len)
+        total_ref_block_len=hl.agg.sum(mt.ref_block_len),
     )
-    ht = mt.annotate_cols(
-        n_called=mt.n_nonref + mt.total_ref_block_len
-    ).cols()
+    ht = mt.annotate_cols(n_called=mt.n_nonref + mt.total_ref_block_len).cols()
 
-    # Calculate callrate and return 
-    ht = ht.annotate(
-        callrate=(ht.total_ref_block_len + ht.n_nonref) / total_bases
-    )
+    # Calculate callrate and return
+    ht = ht.annotate(callrate=(ht.total_ref_block_len + ht.n_nonref) / total_bases)
     return ht
 
 
 # Sites resources
 def get_sites(
-    af_cutoff: float, call_rate_cutoff: float, greater: bool = True,  
-    pass_interval_qc: bool = True, autosomes_only: bool = True
-    ) -> hl.Table:
+    af_cutoff: float,
+    call_rate_cutoff: float,
+    greater: bool = True,
+    pass_interval_qc: bool = True,
+    autosomes_only: bool = True,
+) -> hl.Table:
     """
     Returns a Table of sites that meet af_cutoff from the 200K (tranche 2/freeze 5) callset. 
     
@@ -292,9 +312,9 @@ def get_sites(
         mt = filter_to_autosomes(mt)
 
     if pass_interval_qc:
-        mt = annotate_interval_qc_filter('broad', 5, mt, autosomes_only=autosomes_only)
+        mt = annotate_interval_qc_filter("broad", 5, mt, autosomes_only=autosomes_only)
         mt = mt.filter_rows(mt.interval_qc_pass)
-    
+
     # Add callstats (for AF) and filter on AF cutoff
     mt = hl.variant_qc(mt)
     mt = mt.filter_rows(mt.variant_qc.call_rate > call_rate_cutoff)
@@ -320,16 +340,26 @@ def calculate_fstat_sites() -> None:
     data_source = "broad"
     freeze = 5
     mt = hl.read_matrix_table(raw_mt_path(data_source, freeze, densified=True))
-    mt = mt.key_rows_by('locus', 'alleles')
-    mt = hl.filter_intervals(mt,
-                            [hl.parse_locus_interval(x_contig, reference_genome=get_reference_genome(mt.locus).name)
-                            for x_contig in get_reference_genome(mt.locus).x_contigs])
-    mt = mt.filter_rows((hl.len(mt.alleles) == 2) & hl.is_snp(mt.alleles[0], mt.alleles[1]))
-    mt = annotate_interval_qc_filter('broad', 5, mt, autosomes_only=False)
+    mt = mt.key_rows_by("locus", "alleles")
+    mt = hl.filter_intervals(
+        mt,
+        [
+            hl.parse_locus_interval(
+                x_contig, reference_genome=get_reference_genome(mt.locus).name
+            )
+            for x_contig in get_reference_genome(mt.locus).x_contigs
+        ],
+    )
+    mt = mt.filter_rows(
+        (hl.len(mt.alleles) == 2) & hl.is_snp(mt.alleles[0], mt.alleles[1])
+    )
+    mt = annotate_interval_qc_filter("broad", 5, mt, autosomes_only=False)
     mt = mt.filter_rows(mt.interval_qc_pass)
     mt = mt.transmute_entries(GT=hl.experimental.lgt_to_gt(mt.LGT, mt.LA))
     mt = hl.variant_qc(mt)
-    mt = mt.filter_rows((hl.agg.fraction(hl.is_defined(mt.GT)) > 0.99) & (mt.variant_qc.AF[1] > 0.001))
+    mt = mt.filter_rows(
+        (hl.agg.fraction(hl.is_defined(mt.GT)) > 0.99) & (mt.variant_qc.AF[1] > 0.001)
+    )
     ht = mt.rows()
     ht = ht.transmute(AF=ht.variant_qc.AF[1])
     ht = ht.write(f_stat_sites_path())
@@ -352,13 +382,12 @@ def get_qc_mt_sites() -> None:
 
     logger.info("Removing LCR intervals from QC sites")
     lcr_intervals = lcr.ht()
-    ht = ht.filter(
-            hl.is_missing(lcr[ht.locus])
-    )
+    ht = ht.filter(hl.is_missing(lcr[ht.locus]))
 
     logger.info("Adding info annotations to QC sites HT")
     # NOTE: info ht is hard coded from tranche 2/freeze 5
-    info_ht = hl.read_matrix_table("gs://broad-ukbb/broad.freeze_5/temp/broad.freeze_5.sites.ht")
+    info_ht = hl.read_matrix_table(
+        "gs://broad-ukbb/broad.freeze_5/temp/broad.freeze_5.sites.ht"
+    )
     ht = ht.annotate(info=info_ht[ht.key].info)
     ht.write(qc_sites_path())
-
