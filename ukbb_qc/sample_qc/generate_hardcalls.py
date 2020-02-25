a11c0e25ec7d08f48b185a41bfd2a9fa0d6b06ea
Katherine Chao <kchao@broadinstitute.org>
2020-02-25 08:36:52 -0500

    Reformatted array concordance with black

--- ukbb_qc/sample_qc/generate_hardcalls.py
+++ ukbb_qc/sample_qc/generate_hardcalls.py
@@ -20,76 +20,133 @@ def main(args):
 
     if args.impute_sex:
         logger.info("Imputing sex...")
-        mt = get_ukbb_data(data_source, freeze, split=False, raw=True, key_by_locus_and_alleles=True)
+        mt = get_ukbb_data(
+            data_source, freeze, split=False, raw=True, key_by_locus_and_alleles=True
+        )
 
         # NOTE: Broad callset does not have filter annotations
         if data_source == "regeneron":
             mt = mt.filter_rows(hl.is_missing(mt.filters))
 
         sex_ht = default_annotate_sex(
-            mt, included_intervals=capture_ht_path(data_source, freeze),
-            sites_ht=hl.read_table(f_stats_sites_path()), aaf_expr='AF', gt_expr='LGT')
+            mt,
+            included_intervals=capture_ht_path(data_source, freeze),
+            sites_ht=hl.read_table(f_stats_sites_path()),
+            aaf_expr="AF",
+            gt_expr="LGT",
+        )
         sex_ht = sex_ht.checkpoint(sex_ht_path(data_source, freeze), overwrite=True)
     # NOTE: check distributions here before continuing with hardcalls
 
     if args.write_hardcalls:
         logger.info("Generating split hardcalls...")
-        mt = get_ukbb_data(data_source, freeze, split=False, raw=True, key_by_locus_and_alleles=True)
+        mt = get_ukbb_data(
+            data_source, freeze, split=False, raw=True, key_by_locus_and_alleles=True
+        )
         sex_ht = hl.read_table(sex_ht_path(data_source, freeze))
 
         # Add allele data to mt
-        allele_data = hl.struct(original_alleles=mt.alleles,
-                            has_star=hl.any(lambda a: a == "*", mt.alleles))
-        mt = mt.annotate_rows(allele_data=allele_data.annotate(**add_variant_type(mt.alleles)))
+        allele_data = hl.struct(
+            original_alleles=mt.alleles, has_star=hl.any(lambda a: a == "*", mt.alleles)
+        )
+        mt = mt.annotate_rows(
+            allele_data=allele_data.annotate(**add_variant_type(mt.alleles))
+        )
 
-        # Split mt, add adj annotation, and adjust sex ploidies 
+        # Split mt, add adj annotation, and adjust sex ploidies
         mt = hl.experimental.sparse_split_multi(mt)
         mt = annotate_adj(mt.select_cols(sex_karyotype=ht[mt.col_key].sex_karyotype))
-        mt = mt.select_entries(GT=mt.GT, adj=mt.adj)  # Note: this is different from gnomAD hardcalls file because no PGT or PID
+        mt = mt.select_entries(
+            GT=mt.GT, adj=mt.adj
+        )  # Note: this is different from gnomAD hardcalls file because no PGT or PID
         mt = adjust_sex_ploidy(mt, mt.sex_karyotype, male_str="XY", female_str="XX")
         mt = mt.select_cols().naive_coalesce(args.n_partitions)
-        mt.write(get_ukbb_data_path(data_source, freeze, hardcalls=True, split=False), args.overwrite)
+        mt.write(
+            get_ukbb_data_path(data_source, freeze, hardcalls=True, split=False),
+            args.overwrite,
+        )
 
         # Finish generating allele data
         ht = mt.rows().select("allele_data")
-        allele_type = (hl.case()
-                   .when(hl.is_snp(ht.alleles[0], ht.alleles[1]), "snv")
-                   .when(hl.is_insertion(ht.alleles[0], ht.alleles[1]), "ins")
-                   .when(hl.is_deletion(ht.alleles[0], ht.alleles[1]), "del")
-                   .default("complex")
-                   )
-        ht = ht.annotate(allele_data=ht.allele_data.annotate(allele_type=allele_type,
-                                                              was_mixed=ht.allele_data.variant_type == "mixed"))
-        ht.write(var_annotations_ht_path(data_source, freeze, "allele_data"), args.overwrite)
+        allele_type = (
+            hl.case()
+            .when(hl.is_snp(ht.alleles[0], ht.alleles[1]), "snv")
+            .when(hl.is_insertion(ht.alleles[0], ht.alleles[1]), "ins")
+            .when(hl.is_deletion(ht.alleles[0], ht.alleles[1]), "del")
+            .default("complex")
+        )
+        ht = ht.annotate(
+            allele_data=ht.allele_data.annotate(
+                allele_type=allele_type,
+                was_mixed=ht.allele_data.variant_type == "mixed",
+            )
+        )
+        ht.write(
+            var_annotations_ht_path(data_source, freeze, "allele_data"), args.overwrite
+        )
 
     if args.write_nonrefs:
         logger.info("Creating split sparse MT with only non-ref genotypes...")
-        mt = get_ukbb_data(data_source, freeze, split=False, raw=True, key_by_locus_and_alleles=True).select_cols()
+        mt = get_ukbb_data(
+            data_source, freeze, split=False, raw=True, key_by_locus_and_alleles=True
+        ).select_cols()
         mt = mt.drop("gvcf_info")
         mt = mt.annotate_entries(is_missing=hl.is_missing(mt.LGT))
         mt = mt.filter_entries(mt.is_missing | mt.LGT.is_non_ref())
 
         if data_source == "regeneron":
-            mt = mt.drop("PL")  # Note: I guess we need to check if PL has issues rather than just always dropping
+            mt = mt.drop(
+                "PL"
+            )  # Note: I guess we need to check if PL has issues rather than just always dropping
         mt = hl.experimental.sparse_split_multi(mt)
         mt = mt.filter_entries(mt.is_missing | mt.GT.is_non_ref())
         mt = annotate_adj(mt)
         mt = mt.naive_coalesce(args.n_partitions)
-        mt.write(get_ukbb_data_path(data_source, freeze, split=True, non_refs_only=True), args.overwrite)
+        mt.write(
+            get_ukbb_data_path(data_source, freeze, split=True, non_refs_only=True),
+            args.overwrite,
+        )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-o", "--overwrite", help="Overwrite all data from this subset (default: False)", action="store_true")
-    parser.add_argument("--slack_channel", help="Slack channel to post results and notifications to.")
-    parser.add_argument("-s", "--data_source", help="Data source", choices=["regeneron", "broad"], default="broad")
-    parser.add_argument("-f", "--freeze", help="Data freeze to use", default=CURRENT_FREEZE, type=int)
-    parser.add_argument("--n_partitions", help="Desired number of partitions for output", type=int)
+    parser.add_argument(
+        "-o",
+        "--overwrite",
+        help="Overwrite all data from this subset (default: False)",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--slack_channel", help="Slack channel to post results and notifications to."
+    )
+    parser.add_argument(
+        "-s",
+        "--data_source",
+        help="Data source",
+        choices=["regeneron", "broad"],
+        default="broad",
+    )
+    parser.add_argument(
+        "-f", "--freeze", help="Data freeze to use", default=CURRENT_FREEZE, type=int
+    )
+    parser.add_argument(
+        "--n_partitions", help="Desired number of partitions for output", type=int
+    )
 
-    parser.add_argument("--impute_sex", help="Impute sex on raw MT (prerequisite for creating hardcalls)", action="store_true")
-    parser.add_argument("--write_hardcalls", help="Creates a split hardcalls mt", action="store_true")
-    parser.add_argument("--write_nonrefs", help="Creates a split sparse mt with only non-ref genotypes", action="store_true")
+    parser.add_argument(
+        "--impute_sex",
+        help="Impute sex on raw MT (prerequisite for creating hardcalls)",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--write_hardcalls", help="Creates a split hardcalls mt", action="store_true"
+    )
+    parser.add_argument(
+        "--write_nonrefs",
+        help="Creates a split sparse mt with only non-ref genotypes",
+        action="store_true",
+    )
 
     args = parser.parse_args()
 
--- ukbb_qc/sample_qc/outlier_filter.py
+++ ukbb_qc/sample_qc/outlier_filter.py
@@ -4,13 +4,19 @@ import logging
 from typing import Union
 from gnomad_hail.resources.grch38.intervals import lcr
 from gnomad_hail.utils.generic import (
-                                bi_allelic_expr, filter_low_conf_regions, filter_to_autosomes
-                                )
+    bi_allelic_expr,
+    filter_low_conf_regions,
+    filter_to_autosomes,
+)
 from gnomad_hail.utils.sample_qc import compute_stratified_metrics_filter
 from gnomad_hail.utils.slack import try_slack
 from ukbb_qc.resources.basics import array_sample_map_ht_path, get_ukbb_data
 from ukbb_qc.resources.resource_utils import CURRENT_FREEZE
-from ukbb_qc.resources.sample_qc import ancestry_hybrid_ht_path, platform_pop_outlier_ht_path, qc_temp_data_prefix
+from ukbb_qc.resources.sample_qc import (
+    ancestry_hybrid_ht_path,
+    platform_pop_outlier_ht_path,
+    qc_temp_data_prefix,
+)
 from ukbb_qc.utils import annotate_interval_qc_filter, remove_hard_filter_samples
 
 
@@ -21,7 +27,7 @@ logger.setLevel(logging.INFO)
 
 def filter_low_conf_regions(
     t: Union[hl.MatrixTable, hl.Table], filter_lcr: bool = True
-    ) -> Union[hl.MatrixTable, hl.Table]:
+) -> Union[hl.MatrixTable, hl.Table]:
     """
     Filters low-confidence regions
 
@@ -42,7 +48,11 @@ def filter_low_conf_regions(
 
     if criteria:
         filter_criteria = functools.reduce(operator.iand, criteria)
-        t = t.filter_rows(filter_criteria) if isinstance(t, hl.MatrixTable) else t.filter(filter_criteria)
+        t = (
+            t.filter_rows(filter_criteria)
+            if isinstance(t, hl.MatrixTable)
+            else t.filter(filter_criteria)
+        )
 
     return t
 
@@ -62,8 +72,14 @@ def run_sample_qc(mt: hl.MatrixTable) -> hl.MatrixTable:
 
     logger.info("Starting sample QC...")
     mt = hl.sample_qc(mt)
-    mt = mt.annotate_rows(variant_qc=hl.struct(af=hl.agg.mean(mt.GT.n_alt_alleles()) / 2))
-    mt = mt.annotate_cols(sample_qc=mt.sample_qc.annotate(f_inbreeding=hl.agg.inbreeding(mt.GT, mt.variant_qc.af)))
+    mt = mt.annotate_rows(
+        variant_qc=hl.struct(af=hl.agg.mean(mt.GT.n_alt_alleles()) / 2)
+    )
+    mt = mt.annotate_cols(
+        sample_qc=mt.sample_qc.annotate(
+            f_inbreeding=hl.agg.inbreeding(mt.GT, mt.variant_qc.af)
+        )
+    )
 
     return mt.cols().select("sample_qc")
 
@@ -90,30 +106,36 @@ def main(args):
         mt = remove_hard_filter_samples(data_source, freeze, mt, non_refs_only=False)
         logger.info(f"mt count post filtering: {mt.count()}")
 
-        logger.info("Running mini sample QC for platform- and population-specific filtering...")
+        logger.info(
+            "Running mini sample QC for platform- and population-specific filtering..."
+        )
         sample_qc_ht = run_sample_qc(mt)
         sample_qc_ht.write(
-            qc_temp_data_prefix(data_source, freeze) + "outlier_sample_qc_intervals.ht", 
-            args.overwrite
+            qc_temp_data_prefix(data_source, freeze) + "outlier_sample_qc_intervals.ht",
+            args.overwrite,
         )
 
-    sample_qc_ht = hl.read_table(qc_temp_data_prefix(data_source, freeze) + "outlier_sample_qc_intervals.ht")
+    sample_qc_ht = hl.read_table(
+        qc_temp_data_prefix(data_source, freeze) + "outlier_sample_qc_intervals.ht"
+    )
     strata = {}
-    
+
     logger.info("Annotating platform assignments...")
     # NOTE using tranche as a proxy for platform in freeze 5
-    #platform_ht = hl.read_table(platform_pca_results_ht_path(data_source, freeze))
-    #sample_qc_ht = sample_qc_ht.annotate(qc_platform=platform_ht[sample_qc_ht.key].qc_platform)
+    # platform_ht = hl.read_table(platform_pca_results_ht_path(data_source, freeze))
+    # sample_qc_ht = sample_qc_ht.annotate(qc_platform=platform_ht[sample_qc_ht.key].qc_platform)
     sample_map_ht = hl.read_table(array_sample_map_ht_path(data_source, freeze))
-    
+
     logger.info("Annotating population assignments...")
     pop_ht = hl.read_table(ancestry_hybrid_ht_path(data_source, freeze))
-    sample_qc_ht = sample_qc_ht.annotate(qc_pop=pop_ht[sample_qc_ht.key][pop_assignment_method])
+    sample_qc_ht = sample_qc_ht.annotate(
+        qc_pop=pop_ht[sample_qc_ht.key][pop_assignment_method]
+    )
 
     if not args.skip_platform_filter:
         strata["qc_pop"] = sample_qc_ht.qc_pop
     if not args.skip_population_filter:
-        #strata["qc_platform"] = sample_qc_ht.qc_platform
+        # strata["qc_platform"] = sample_qc_ht.qc_platform
         strata["qc_platform"] = sample_qc_ht.batch
     else:
         pop_assignment_method = None
@@ -125,42 +147,81 @@ def main(args):
         qc_metrics[m] = sample_qc_ht.sample_qc[f"{m}"]
 
     # For each platform and population, aggregate sample QC metrics and calculate the MAD/mean/stdev
-    logger.info("Flagging samples failing pop/platform-specific sample qc thresholds...")
+    logger.info(
+        "Flagging samples failing pop/platform-specific sample qc thresholds..."
+    )
     pop_platform_filter_ht = compute_stratified_metrics_filter(
-        sample_qc_ht,
-        qc_metrics,
-        strata
+        sample_qc_ht, qc_metrics, strata
     )
     pop_platform_filter_ht.write(
-        platform_pop_outlier_ht_path(data_source, freeze, pop_assignment_method), 
-        overwrite=args.overwrite
+        platform_pop_outlier_ht_path(data_source, freeze, pop_assignment_method),
+        overwrite=args.overwrite,
     )
     num_pass = pop_platform_filter_ht.aggregate(
         hl.agg.count_where(hl.len(pop_platform_filter_ht.qc_metrics_filters) == 0)
     )
-    logger.info(f"{num_pass} exome samples found passing pop/platform-specific filtering")
+    logger.info(
+        f"{num_pass} exome samples found passing pop/platform-specific filtering"
+    )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-o", "--overwrite", help="Overwrite all data from this subset (default: False)", action="store_true")
-    parser.add_argument("--slack_channel", help="Slack channel to post results and notifications to.")
-    parser.add_argument("-s", "--data_source", help="Data source", choices=["regeneron", "broad"], default="broad")
-    parser.add_argument("-f", "--freeze", help="Data freeze to use", default=CURRENT_FREEZE, type=int)
+    parser.add_argument(
+        "-o",
+        "--overwrite",
+        help="Overwrite all data from this subset (default: False)",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--slack_channel", help="Slack channel to post results and notifications to."
+    )
+    parser.add_argument(
+        "-s",
+        "--data_source",
+        help="Data source",
+        choices=["regeneron", "broad"],
+        default="broad",
+    )
+    parser.add_argument(
+        "-f", "--freeze", help="Data freeze to use", default=CURRENT_FREEZE, type=int
+    )
 
-    parser.add_argument("--run_mini_qc",
-                                      help="Run mini sample qc needed for outlier filtering",
-                                      action="store_true")
-    parser.add_argument("--skip_platform_filter",
-                                      help="Skip including platforms in outlier filtering",
-                                      action="store_true")
-    parser.add_argument("--skip_population_filter",
-                                      help="Skip including population in outlier filtering",
-                                      action="store_true")
-    parser.add_argument("--pop_assignment_method", help="Population assignment method to use for stratification",
-                        default="hybrid_pop", choices=["gnomad_pc_project_pop","HDBSCAN_pop_cluster","hybrid_pop"])
-    parser.add_argument("--filtering_qc_metrics", help="List of QC metrics for filtering.", default=",".join(
-    ["n_snp", "r_ti_tv", "r_insertion_deletion", "n_insertion", "n_deletion", "r_het_hom_var"]))
+    parser.add_argument(
+        "--run_mini_qc",
+        help="Run mini sample qc needed for outlier filtering",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--skip_platform_filter",
+        help="Skip including platforms in outlier filtering",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--skip_population_filter",
+        help="Skip including population in outlier filtering",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--pop_assignment_method",
+        help="Population assignment method to use for stratification",
+        default="hybrid_pop",
+        choices=["gnomad_pc_project_pop", "HDBSCAN_pop_cluster", "hybrid_pop"],
+    )
+    parser.add_argument(
+        "--filtering_qc_metrics",
+        help="List of QC metrics for filtering.",
+        default=",".join(
+            [
+                "n_snp",
+                "r_ti_tv",
+                "r_insertion_deletion",
+                "n_insertion",
+                "n_deletion",
+                "r_het_hom_var",
+            ]
+        ),
+    )
 
     args = parser.parse_args()
 
