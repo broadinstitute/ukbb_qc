6de0d8093a3a61a24e85dce69bbb66e2e2cd00be
Katherine Chao <kchao@broadinstitute.org>
2020-02-26 09:42:11 -0500

    Added function to compute callrate/mean dp mt to sparse_utils

--- ukbb_qc/utils/sparse_utils.py
+++ ukbb_qc/utils/sparse_utils.py
@@ -1,4 +1,76 @@
+import logging
+from gnomad_hail.utils.generic import bi_allelic_expr, filter_to_autosomes
 from ukbb_qc.resources.sample_qc import interval_qc_path
+
+
+logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
+logger = logging.getLogger("sparse_utils")
+logger.setLevel(logging.INFO)
+
+
+def compute_callrate_dp_mt(
+    data_source: str,
+    freeze: int,
+    mt: hl.MatrixTable,
+    intervals_ht: hl.Table,
+    bi_allelic_only: bool = True,
+    autosomes_only: bool = True,
+    match: bool = True,
+) -> None:
+    """
+    Computes sample n_defined and n_count (inputs to callrate) and mean depth per interval.
+    Mean depth annotation is used during interval QC.
+    Callrate annotations are used during platform PCA.
+    Writes call rate mt (Grouped MatrixTable) keyed by intervals row-wise and samples column-wise.
+    NOTE: This function requires a densify! Please use an autoscaling cluster.
+
+    :param data_source: One of 'regeneron' or 'broad'
+    :param int freeze: One of the data freezes
+    :param MatrixTable mt: Input MatrixTable.
+    :param Table intervals_ht: Table with capture intervals relevant to input MatrixTable.
+    :param bi_allelic_only: If set, only bi-allelic sites are used for the computation.
+    :param autosomes_only: If set, only autosomal intervals are used.
+    :param matches: If set, returns all intervals in interval_ht that overlap the locus in the input MT.
+    :return: None
+    """
+    logger.info("Computing call rate and mean DP MatrixTable")
+    if len(intervals_ht.key) != 1 or not isinstance(
+        intervals_ht.key[0], hl.expr.IntervalExpression
+    ):
+        logger.warning(
+            f"Call rate matrix computation expects `intervals_ht` with a key of type Interval. Found: {intervals_ht.key}"
+        )
+    logger.info("Densifying...")
+    mt = hl.experimental.densify(mt)
+
+    logger.info(
+        "Filtering out lines that are only reference or not covered in capture intervals"
+    )
+    mt = mt.filter_rows(hl.len(mt.alleles) > 1)
+    intervals_ht = intervals_ht.annotate_rows(interval_label=intervals_ht.interval)
+    mt = mt.annotate_rows(
+        interval=intervals_ht.index(mt.locus, all_matches=match).interval_label
+    )
+    mt = mt.filter_rows(hl.is_defined(mt.interval))
+
+    if autosomes_only:
+        mt = filter_to_autosomes(mt)
+
+    if bi_allelic_only:
+        mt = mt.filter_rows(bi_allelic_expr(mt))
+
+    if match:
+        mt = mt.explode_rows("interval")
+
+    logger.info(
+        "Grouping MT by interval and calculating n_defined, n_count, and mean_dp..."
+    )
+    mt = mt.group_rows_by(**_mt._interval_key).aggregate(
+        n_defined=hl.agg.count_where(hl.is_defined(mt.GT)),
+        count=hl.agg.count(),
+        mean_dp=hl.agg.mean(hl.if_else(hl.is_defined(mt.DP), mt.DP, 0)),
+    )
+    mt.write(callrate_mt_path(data_source, freeze, interval_filtered=False))
 
 
 def get_sparse_sample_qc(mt: hl.MatrixTable, autosomes_only: bool = True) -> hl.Table:
