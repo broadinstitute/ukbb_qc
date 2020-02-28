3729e00d69ab32f6f944c438a08bd788571eeb1d
Katherine Chao <kchao@broadinstitute.org>
2020-02-28 11:48:17 -0500

    Changed qc ht code to stop storing GQ stats

--- ukbb_qc/sample_qc/process_raw_data.py
+++ ukbb_qc/sample_qc/process_raw_data.py
@@ -197,7 +197,7 @@ def main(args):
         qc_mt = filter_to_autosomes(qc_mt)
         qc_ht = hl.sample_qc(qc_mt).cols().select("sample_qc")
         qc_ht = qc_ht.transmute(
-            sample_qc=qc_ht.sample_qc.select("call_rate", "gq_stats", "dp_stats")
+            sample_qc=qc_ht.sample_qc.select("call_rate", "dp_stats")
         )
         qc_ht.write(qc_ht_path(data_source, freeze), overwrite=args.overwrite)
 
