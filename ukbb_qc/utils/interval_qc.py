b29cc4b3c5bc031bf34c6c52f8bea8bf7d1a9824
Katherine Chao <kchao@broadinstitute.org>
2020-03-11 12:03:21 -0400

    Removed option to read regeneron data from interval qc, also reformatted with black

--- ukbb_qc/utils/interval_qc.py
+++ ukbb_qc/utils/interval_qc.py
@@ -4,11 +4,11 @@ import logging
 from gnomad.utils.generic import filter_to_autosomes
 from gnomad.utils.slack import try_slack
 from ukbb_qc.resources.basics import (
-    CURRENT_FREEZE,
     capture_ht_path,
     get_checkpoint_path,
     get_ukbb_data,
 )
+from ukbb_qc.resources.resource_utils import CURRENT_FREEZE
 from ukbb_qc.resources.sample_qc import interval_qc_path, sex_ht_path
 from ukbb_qc.utils.sparse_utils import compute_callrate_dp_mt
 
@@ -19,9 +19,7 @@ logger.setLevel(logging.INFO)
 
 
 def interval_qc(
-    target_mt: hl.MatrixTable,
-    split_by_sex: bool = False,
-    ht_n_partitions: int = 100
+    target_mt: hl.MatrixTable, split_by_sex: bool = False, ht_n_partitions: int = 100,
 ) -> hl.Table:
     """
     Determines the percent of samples reaching 10x, 15x, 20x, 25x, and 30x mean coverage in each interval
@@ -84,23 +82,27 @@ def main(args):
     if not args.autosomes and not args.sex_chr:
         logger.warning("Must choose one of autosomes or sex_chr options")
 
-    data_source = args.data_source
+    data_source = "broad"
     freeze = args.freeze
 
     # NOTE: this function will densify
     if args.compute_callrate_mt:
         logger.info("Reading in raw MT...")
         mt = get_ukbb_data(
-            data_source, freeze, raw=True, adj=False, key_by_locus_and_alleles=True
+            data_source, freeze, raw=True, split=False, key_by_locus_and_alleles=True,
         )
         logger.info(
             f"Total number of variants in raw unsplit matrix table: {mt.count_rows()}"
         )
         capture_ht = hl.read_table(capture_ht_path(data_source))
-        compute_callrate_dp_mt(data_source, freeze, mt, capture_ht, autosomes_only=False)
+        compute_callrate_dp_mt(
+            data_source, freeze, mt, capture_ht, autosomes_only=False,
+        )
 
     logger.info("Reading in call rate MT...")
-    mt = hl.read_matrix_table(callrate_mt_path(data_source, freeze, interval_filtered=False))
+    mt = hl.read_matrix_table(
+        callrate_mt_path(data_source, freeze, interval_filtered=False)
+    )
     if args.autosomes:
         logger.info("Filtering to autosomes...")
         mt = filter_to_autosomes(mt)
@@ -128,30 +130,21 @@ def main(args):
         mt = mt.filter_cols((mt.sex_karyotype == "XX") | (mt.sex_karyotype == "XY"))
 
         logger.info("Starting interval QC...")
-        target_ht = interval_qc(
-            mt,
-            split_by_sex=True
-        )
+        target_ht = interval_qc(mt, split_by_sex=True)
         target_ht.write(
             interval_qc_path(data_source, freeze, "sex_chr"), overwrite=args.overwrite,
         )
 
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-s",
-        "--data_source",
-        help="Data source",
-        choices=["regeneron", "broad"],
-        default="broad",
-    )
     parser.add_argument(
         "-f", "--freeze", help="Data freeze to use", default=CURRENT_FREEZE, type=int,
     )
 
     parser.add_argument(
-        "--n_partitions", help="Desired number of partitions for output", type=int
+        "--n_partitions", help="Desired number of partitions for output", type=int,
     )
 
     parser.add_argument(
@@ -165,7 +158,7 @@ if __name__ == "__main__":
         action="store_true",
         help="If set it will only run the sex chromosomes",
     )
-        parser.add_argument(
+    parser.add_argument(
         "--compute_callrate_mt",
         help="Computes an interval by sample mt of callrate and depth",
         action="store_true",
