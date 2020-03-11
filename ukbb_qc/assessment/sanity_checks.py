97005b4e4fcf06dcc9dc736f6e41f7771625bb78
Katherine Chao <kchao@broadinstitute.org>
2020-03-11 16:28:34 -0400

    Updated sample check in sanity_checks to be more flexible with the shows and to return a bool

--- ukbb_qc/assessment/sanity_checks.py
+++ ukbb_qc/assessment/sanity_checks.py
@@ -53,7 +53,9 @@ def check_adj(mt: hl.MatrixTable, mt_adj: hl.MatrixTable) -> bool:
     return adj
 
 
-def sample_check(ht: hl.Table, exp_ht: hl.Table, sample_qc_path: str) -> None:
+def sample_check(
+    ht: hl.Table, exp_ht: hl.Table, sample_qc_path: str, show_mismatch: bool = True,
+) -> bool:
     """
     Checks for sample mismatch between samples in two Tables.
     If there is a sample mismatch, writes unique samples to output txt file
@@ -62,20 +64,31 @@ def sample_check(ht: hl.Table, exp_ht: hl.Table, sample_qc_path: str) -> None:
     :param Table ht: Table containing samples to be checked
     :param Table exp_ht: Table with one column containing expected samples
     :param str sample_qc_path: Path to output sample_qc bucket (for output txts)
-    :return: None
-    :rtype: None
+    :param bool show_mismatch: Boolean whether to print sample mismatches to stdout. Default is True
+    :return: Bool for whether there was a sample_mismatch
+    :rtype: bool
     """
+    sample_mismatch = False
+
     missing_samples = exp_ht.anti_join(ht).select()
     n_missing_samples = missing_samples.count()
     extra_samples = ht.anti_join(exp_ht).select()
     n_extra_samples = extra_samples.count()
 
-    logger.info(
-        f"Total number of expected IDs that are not in the sample HT: {n_missing_samples}..."
-    )
-    missing_samples.show(n_missing_samples)
+    if n_missing_samples > 0:
+        sample_mismatch = True
+        logger.info(
+            f"Total number of expected IDs that are not in the sample HT: {n_missing_samples}..."
+        )
+        if show_mismatch:
+            missing_samples.show(n_missing_samples)
 
-    logger.info(
-        f"Total number of extra IDs that were not in the expected HT: {n_extra_samples}..."
-    )
-    extra_samples.show(n_extra_samples)
+    if n_extra_samples > 0:
+        sample_mismatch = True
+        logger.info(
+            f"Total number of extra IDs that were not in the expected HT: {n_extra_samples}..."
+        )
+        if show_mismatch:
+            extra_samples.show(n_extra_samples)
+
+    return sample_mismatch
