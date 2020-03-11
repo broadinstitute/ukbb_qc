abe3739118f70178875194a4b90261d545f13bf4
Katherine Chao <kchao@broadinstitute.org>
2020-03-11 11:53:23 -0400

    Removed apply hard filters from process raw

--- ukbb_qc/sample_qc/process_raw_data.py
+++ ukbb_qc/sample_qc/process_raw_data.py
@@ -4,23 +4,16 @@ import logging
 from gnomad.utils.generic import filter_to_autosomes
 from gnomad.utils.gnomad_functions import filter_to_adj
 from gnomad.utils.sample_qc import get_qc_mt
-from gnomad.utils.sparse_mt import compute_last_ref_block_end, densify_sites
+from gnomad.utils.sparse_mt import densify_sites
 from ukbb_qc.resources.basics import (
-    array_sample_map_ht_path,
     get_checkpoint_path,
     get_ukbb_data,
+    last_END_positions_ht_path,
 )
 from ukbb_qc.resources.resource_utils import CURRENT_FREEZE
-from ukbb_qc.resources.sample_qc import (
-    callrate_mt_path,
-    interval_qc_path,
-    sex_ht_path,
-    qc_mt_path,
-)
+from ukbb_qc.resources.sample_qc import qc_mt_path
 from ukbb_qc.resources.variant_qc import get_truth_sample_info
-from ukbb_qc.sample_qc.apply_hard_filters import hard_filter_samples
-from ukbb_qc.utils.sparse_utils import compute_callrate_dp_mt
-from ukbb_qc.utils.utils import annotate_interval_qc_filter, get_qc_mt_sites
+from ukbb_qc.utils.utils import get_qc_mt_sites
 
 
 logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
@@ -31,59 +24,13 @@ logger.setLevel(logging.INFO)
 def main(args):
     hl.init(log="/process_raw_data.log", default_reference="GRCh38")
 
-    data_source = args.data_source
+    data_source = "broad"
     freeze = args.freeze
-
-    if args.apply_hard_filters:
-
-        # NOTE: this function will densify
-        if args.compute_callrate_mt:
-            logger.info("Reading in raw MT...")
-            mt = get_ukbb_data(
-                data_source, freeze, raw=True, adj=False, key_by_locus_and_alleles=True
-            )
-            logger.info(
-                f"Total number of variants in raw unsplit matrix table: {mt.count_rows()}"
-            )
-            capture_ht = hl.read_table(capture_ht_path(data_source))
-            compute_callrate_dp_mt(data_source, freeze, mt, capture_ht)
-
-        logger.info("Reading in callrate MT, sex ht, interval qc HT...")
-        callrate_mt = hl.read_matrix_table(
-            callrate_mt_path(data_source, freeze, interval_filtered=False)
-        )
-        sex_ht = hl.read_table(sex_ht_path(data_source, freeze))
-        interval_qc_ht = hl.read_table(
-            interval_qc_path(data_source, freeze, "autosomes")
-        )
-        interval_qc_ht = interval_qc_ht.filter(
-            interval_qc_ht[args.cov_filter_field] > args.pct_samples
-        )
-
-        logger.info("Hard filtering samples...")
-        hard_filters_ht = hard_filter_samples(
-            data_source,
-            freeze,
-            callrate_mt,
-            interval_qc_ht,
-            sex_ht,
-            args.min_callrate,
-            args.min_dp,
-        )
-        ht = ht.naive_coalesce(args.n_partitions)
-        ht = ht.checkpoint(
-            hard_filters_ht_path(data_source, freeze), overwrite=args.overwrite
-        )
-        logger.info("Checking number of samples flagged with hard filters...")
-        ht = ht.explode(ht.hard_filters)
-        filters = ht.aggregate(hl.agg.counter(ht.hard_filters))
-        for filt in filters:
-            logger.info(f"Samples flagged due to {filt}: {filters[filt]}")
 
     if args.compute_qc_mt:
         logger.info("Reading in raw MT...")
         mt = get_ukbb_data(
-            data_source, freeze, raw=True, adj=False, key_by_locus_and_alleles=True
+            data_source, freeze, raw=True, adj=False, key_by_locus_and_alleles=True,
         )
         mt = mt.select_entries(
             "DP", GT=mt.LGT, adj=get_adj_expr(mt.LGT, mt.GQ, mt.DP, mt.LAD)
@@ -103,15 +50,15 @@ def main(args):
 
         logger.info("Checkpointing densified MT")
         mt = mt.checkpoint(
-            get_checkpoint_path(data_source, freeze, name="dense", mt=True),
+            get_checkpoint_path(data_source, freeze, name="dense", mt=True,),
             overwrite=True,
         )
 
         logger.info("Repartitioning densified MT")
-        mt = mt.naive_coalesce(n_partitions)
+        mt = mt.naive_coalesce(args.n_partitions)
         mt = mt.checkpoint(
             get_checkpoint_path(
-                data_source, freeze, name="dense.repartitioned", mt=True
+                data_source, freeze, name="dense.repartitioned", mt=True,
             ),
             overwrite=True,
         )
@@ -122,7 +69,7 @@ def main(args):
         mt = mt.annotate_rows(info=info_expr)
         mt = mt.annotate_rows(
             lowqual=get_lowqual_expr(
-                mt.alleles, mt.info.QUALapprox, indel_phred_het_prior=40
+                mt.alleles, mt.info.QUALapprox, indel_phred_het_prior=40,
             )
         )
         mt = filter_to_adj(mt)
@@ -138,24 +85,19 @@ def main(args):
             overwrite=True,
         )
 
-        if data_source == "regeneron":
-            apply_hard_filters = False
-        else:
-            apply_hard_filters = True
-
         # NOTE: adding decoy and segdup hg38 resources/filters are still pending
         qc_mt = get_qc_mt(
             mt,
             min_af=args.min_af,
             min_callrate=args.min_callrate,
-            apply_hard_filters=apply_hard_filters,
+            apply_hard_filters=True,
             ld_r2=None,
             filter_lcr=False,
             filter_decoy=False,
             filter_segdup=False,
         )
         qc_mt = qc_mt.checkpoint(
-            qc_mt_path(data_source, freeze, ld_pruned=True), overwrite=args.overwrite
+            qc_mt_path(data_source, freeze, ld_pruned=True), overwrite=args.overwrite,
         )
         logger.info(
             f"Total number of bi-allelic, high-callrate, common SNPs for sample QC: {qc_mt.count_rows()}"
@@ -174,7 +116,7 @@ def main(args):
         qc_ht.write(qc_ht_path(data_source, freeze), overwrite=args.overwrite)
 
     if args.extract_truth_samples:
-        mt = get_ukbb_data(data_source, freeze, raw=True, adj=False, split=False)
+        mt = get_ukbb_data(data_source, freeze, raw=True, adj=False, split=False,)
         truth_samples = get_truth_sample_info(data_source, freeze)
         samples = [s["s"] for s in truth_samples.values()]
         mt = mt.filter_cols(hl.literal(samples).contains(mt.s))
@@ -193,7 +135,7 @@ def main(args):
             mt = hl.experimental.sparse_split_multi(mt)
             mt = mt.filter_rows(hl.agg.any(mt.GT.is_non_ref()))
             mt.naive_coalesce(args.n_partitions).write(
-                truth_samples[truth_sample]["mt"], overwrite=args.overwrite
+                truth_samples[truth_sample]["mt"], overwrite=args.overwrite,
             )
 
 
@@ -201,42 +143,12 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-s",
-        "--data_source",
-        help="Data source",
-        choices=["regeneron", "broad"],
-        default="broad",
-    )
-    parser.add_argument(
         "-f", "--freeze", help="Data freeze to use", default=CURRENT_FREEZE, type=int,
-    )
-    parser.add_argument(
-        "--apply_hard_filters".help="Apply hard filters to samples",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--compute_callrate_mt",
-        help="Computes an interval by sample mt of callrate and depth that will be for hard filtering samples",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--min-dp", help="Minimum depth", default=20.0, type=float,
     )
     parser.add_argument(
         "--compute_qc_mt",
         help="Compute matrix to be used in sample qc",
         action="store_true",
-    )
-    parser.add_argument(
-        "--apply_interval_qc_filter",  # NOTE not super sure this is necessary
-        help="Filter to good intervals from interval QC",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--min_callrate",
-        help="Minimum variant callrate to retain variant in qc matrix table.",
-        default=0.99,
-        type=float,
     )
     parser.add_argument(
         "--min_af",
@@ -245,14 +157,9 @@ if __name__ == "__main__":
         type=float,
     )
     parser.add_argument(
-        "--cov_filter_field",
-        help="Coverage field to use to filter high coverage intervals",
-        default="pct_samples_20x",
-    )
-    parser.add_argument(
-        "--pct_samples_20x",
-        help="Percent samples at 20X to filter intervals",
-        default=0.85,
+        "--min_callrate",
+        help="Minimum variant callrate to retain variant in qc matrix table.",
+        default=0.99,
         type=float,
     )
     parser.add_argument(
