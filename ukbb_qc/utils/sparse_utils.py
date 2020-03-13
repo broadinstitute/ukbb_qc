addb2a93390bae2eed4dcb7d0fc69581d6db6054
Katherine Chao <kchao@broadinstitute.org>
2020-03-13 11:16:07 -0400

    Added target coverage argument to compute callrate mt function

--- ukbb_qc/utils/sparse_utils.py
+++ ukbb_qc/utils/sparse_utils.py
@@ -16,10 +16,11 @@ def compute_callrate_dp_mt(
     bi_allelic_only: bool = True,
     autosomes_only: bool = True,
     match: bool = True,
+    target_coverage: int = 20,
 ) -> None:
     """
     Computes sample metrics (n_defined, total, mean_dp, pct_gt_20x, pct_dp_defined) per interval. 
-    Mean depth, pct_gt_20x, and pct_dp_defined annotations are used during interval QC.
+    Mean depth, pct_gt_{target_coverage}x, and pct_dp_defined annotations are used during interval QC.
     Mean depth and callrate annotations (mean_dp, n_defined, total) are used during hard filtering.
     Callrate annotations (n_defined, total) are also used during platform PCA.
     Writes call rate mt (aggregated MatrixTable) keyed by intervals row-wise and samples column-wise.
@@ -32,9 +33,13 @@ def compute_callrate_dp_mt(
     :param bi_allelic_only: If set, only bi-allelic sites are used for the computation.
     :param autosomes_only: If set, only autosomal intervals are used.
     :param matches: If set, returns all intervals in interval_ht that overlap the locus in the input MT.
+    :param int target_coverage: Coverage level to check for each target. Default is 20.
     :return: None
     """
-    logger.info("Computing call rate and mean DP MatrixTable")
+    logger.warning(
+        "This function will densify! Make sure you have an autoscaling cluster."
+    )
+    logger.info("Computing call rate and mean DP MatrixTable...")
     if len(intervals_ht.key) != 1 or not isinstance(
         intervals_ht.key[0], hl.expr.IntervalExpression
     ):
@@ -70,15 +75,15 @@ def compute_callrate_dp_mt(
     )
     mt = mt.select_entries(
         GT=hl.or_missing(hl.is_defined(mt.GT), hl.struct()),
-        DP=hl.if_else(hl.is_defined(mt.DP), mt.DP, 0)
+        DP=hl.if_else(hl.is_defined(mt.DP), mt.DP, 0),
     )
     mt = mt.group_rows_by(mt.interval).aggregate(
         n_defined=hl.agg.count_where(hl.is_defined(mt.GT)),
         total=hl.agg.count(),
         dp_sum=hl.agg.sum(mt.DP),
         mean_dp=hl.agg.mean(mt.DP),
-        pct_gt_20x=hl.agg.fraction(mt.DP >= 20),
-        pct_dp_defined=hl.agg.count_where(mt.DP > 0) / hl.agg.count()
+        **{f"pct_gt_{target_coverage}x": hl.agg.fraction(mt.DP >= target_coverage)},
+        pct_dp_defined=hl.agg.count_where(mt.DP > 0) / hl.agg.count(),
     )
     mt.write(callrate_mt_path(data_source, freeze, interval_filtered=False))
 
