b405da1b2b190ff236771dcbefe10e733fb21539
Katherine Chao <kchao@broadinstitute.org>
2020-03-25 11:22:18 -0400

    Updated vqsr vcf path to vqsr sites path to be more flexible (write vcf and ht)

--- ukbb_qc/resources/basics.py
+++ ukbb_qc/resources/basics.py
@@ -404,16 +404,16 @@ def release_vcf_path(data_source: str, freeze: int, contig: str = None) -> str:
         return f"{get_release_path(data_source, freeze)}/vcf/sharded_vcf/{data_source}.freeze_{freeze}.bgz"
 
 
-def vqsr_vcf_path(data_source: str, freeze: int = CURRENT_FREEZE) -> str:
+def vqsr_sites_path(data_source: str, freeze: int = CURRENT_FREEZE) -> str:
     """
-    Fetch bucket for sites VCF (input to VQSR)
+    Fetch bucket for sites HT or VCF (input to VQSR)
 
     :param str data_source: 'regeneron' or 'broad'
     :param int freeze: One of the data freezes
-    :return: Path to VQSR VCF bucket
+    :return: Path to VQSR sites bucket
     :rtype: str
     """
-    return f"{get_release_path(data_source, freeze)}/vqsr/{data_source}.freeze_{freeze}.sites_for_vqsr.vcf.bgz"
+    return f"{get_release_path(data_source, freeze)}/vqsr"
 
 
 # logging path
