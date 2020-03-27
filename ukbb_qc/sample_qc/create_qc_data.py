b8bf6d2be56967a8562e985c9631d8b903397ffc
Katherine Chao <kchao@broadinstitute.org>
2020-03-27 14:04:56 -0400

    Removed all counts from create qc data

--- ukbb_qc/sample_qc/create_qc_data.py
+++ ukbb_qc/sample_qc/create_qc_data.py
@@ -40,15 +40,11 @@ def main(args):
             n_partitions=args.raw_partitions,
         )
         mt = mt.select_entries("LGT", "GQ", "DP", "LAD", "LA", "END")
-        logger.info(
-            f"Total number of variants in raw unsplit matrix table: {mt.count_rows()}"
-        )
 
         logger.info("Reading in QC MT sites from tranche 2/freeze 5...")
         if not hl.utils.hadoop_exists(f"{qc_sites_path()}/_SUCCESS"):
             get_qc_mt_sites()
         qc_sites_ht = hl.read_table(qc_sites_path())
-        logger.info(f"Number of QC sites: {qc_sites_ht.count()}")
 
         logger.info("Densifying sites...")
         last_END_ht = hl.read_table(last_END_positions_ht_path(freeze))
