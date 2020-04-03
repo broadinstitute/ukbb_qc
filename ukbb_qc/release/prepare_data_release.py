29e978372a1a393846610165206f399696a5b1ed
Katherine Chao <kchao@broadinstitute.org>
2020-04-03 16:12:58 -0400

    Updated some imports in release code

--- ukbb_qc/release/prepare_data_release.py
+++ ukbb_qc/release/prepare_data_release.py
@@ -1,22 +1,35 @@
 import argparse
-import hail as hl
 import logging
+import pickle
+from typing import Dict, List, Union
+import hail as hl
+from gnomad.resources.grch37.gnomad import POP_NAMES
 from gnomad.resources.grch38.intervals import lcr
 from gnomad.resources.grch38.reference_data import dbsnp
+from gnomad.utils.sample_qc import add_filters_expr
+from gnomad.utils.generic import get_reference_genome, rep_on_read
+from gnomad.utils.gnomad_functions import filter_to_adj
+from gnomad.utils.annotations import qual_hist_expr, age_hists_expr
 from gnomad_qc.v2.variant_qc.prepare_data_release import (
+    FAF_POPS,
+    GROUPS,
+    HISTS,
+    POPS,
+    SEXES,
+    SORT_ORDER,
     generic_field_check,
     index_globals,
     make_filters_sanity_check_expr,
     make_label_combos,
 )
-from gnomad.utils.sample_qc import add_filters_expr
-from gnomad.utils.generic import get_reference_genome, rep_on_read
-from gnomad.utils.gnomad_functions import filter_to_adj
-from gnomad.utils.annotations import qual_hist_expr, age_hists_expr
-from typing import Dict, List, Union
+from gnomad_qc.v2.variant_qc.prepare_data_release import (
+    EAS_SUBPOPS as GNOMAD_EAS_SUBPOPS,
+)
+from gnomad_qc.v2.variant_qc.prepare_data_release import (
+    NFE_SUBPOPS as GNOMAD_NFE_SUBPOPS,
+)
 from ukbb_qc.resources import *
 from ukbb_qc.utils.constants import *
-import pickle
 
 
 logging.basicConfig(
@@ -754,10 +767,10 @@ def make_combo_header_text(
         header_text = header_text + " samples"
 
     if "subpop" in combo_dict.keys():
-        header_text = header_text + f" of {pop_names[combo_dict['subpop']]} ancestry"
+        header_text = header_text + f" of {POP_NAMES[combo_dict['subpop']]} ancestry"
         combo_dict.pop("pop")
     if "pop" in combo_dict.keys():
-        header_text = header_text + f" of {pop_names[combo_dict['pop']]} ancestry"
+        header_text = header_text + f" of {POP_NAMES[combo_dict['pop']]} ancestry"
 
     if "gnomad" not in prefix:
         header_text = header_text
