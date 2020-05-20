77ba7564967efba2d0d996560a9e49fe53610369
jkgoodrich <jgoodric@broadinstitute.org>
2020-05-20 10:58:52 -0600

    Adding changes to only keep non-refs

--- ukbb_qc/annotations/generate_variantqc_annotations.py
+++ ukbb_qc/annotations/generate_variantqc_annotations.py
@@ -36,8 +36,8 @@ def main(args):
         # Need to spin up a cluster with  --requester-pays-allow-buckets gs://hail-us-vep
         # (or --requester-pays-allows-all) in addition to --vep (build)
         logger.info(f"Running VEP on split hard call MT...")
-        ht = get_ukbb_data(data_source, freeze).rows()
-        ht = ht.filter(hl.len(ht.alleles) > 1)
+        mt = get_ukbb_data(data_source, freeze)
+        ht = mt.filter((hl.len(mt.alleles) > 1) & hl.agg.any(mt.GT.is_non_ref())).rows()
         ht = vep_or_lookup_vep(ht)
         ht = ht.annotate(vep_csq=vep_struct_to_csq(ht.vep))
         ht.naive_coalesce(n_partitions).write(
@@ -48,7 +48,7 @@ def main(args):
     if args.generate_allele_counts:
         logger.info("Computing allele counts on split hard call MT...")
         mt = get_ukbb_data(data_source, freeze, meta_root="meta")
-        mt = mt.filter_rows(hl.len(mt.alleles) > 1)
+        mt = mt.filter((hl.len(mt.alleles) > 1) & hl.agg.any(mt.GT.is_non_ref()))
         ac_expr = {
             "ac_qc_samples_raw": mt.meta.sample_filters.high_quality
             | mt.meta.sample_filters.control,
