973c822b370986c6164669f20001d4eea7fc9c13
Katherine Chao <kchao@broadinstitute.org>
2020-05-27 15:39:05 -0400

    Addressed review comments

--- ukbb_qc/annotations/generate_frequency_data.py
+++ ukbb_qc/annotations/generate_frequency_data.py
@@ -1,33 +1,27 @@
 import argparse
 import logging
-from typing import List
+from typing import List, Optional
 
 import hail as hl
 
 from gnomad.resources.grch37.gnomad import liftover
 from gnomad.resources.grch38.gnomad import public_release
-from gnomad.sample_qc.sex import adjusted_sex_ploidy_expr
 from gnomad.slack_creds import slack_token
 from gnomad.utils.annotations import (
-    age_hists_expr,
     annotate_freq,
     bi_allelic_site_inbreeding_expr,
-    get_adj_expr,
     faf_expr,
     pop_max_expr,
-    qual_hist_expr,
 )
 from gnomad.utils.file_utils import write_temp_gcs
-from gnomad.utils.filtering import filter_to_adj
 from gnomad.utils.slack import slack_notifications
 from ukbb_qc.resources.basics import (
-    array_sample_map_ht_path,
     capture_ht_path,
     get_ukbb_data,
 )
 from ukbb_qc.resources.resource_utils import CURRENT_FREEZE
 from ukbb_qc.resources.variant_qc import var_annotations_ht_path
-from ukbb_qc.utils.utils import get_age_ht
+from ukbb_qc.utils.utils import get_hists
 
 
 logging.basicConfig(
@@ -38,65 +32,16 @@ logger = logging.getLogger("generate_frequency_data")
 logger.setLevel(logging.INFO)
 
 
-def get_hists(mt: hl.MatrixTable, freeze: int) -> hl.MatrixTable:
-    """
-    Gets age (at recruitment; field 21022) and qual hists for UKBB
-
-    :param MatrixTable mt: Input MatrixTable
-    :param int freeze: One of the data freezes
-    :return: MatrixTable with age and qual hists
-    :rtype: MatrixTable
-    """
-    logger.info("Getting age hists")
-    age_ht = get_age_ht(freeze)
-    mt = mt.annotate_cols(age=age_ht[mt.col_key].age)
-    mt = mt.annotate_rows(**age_hists_expr(mt.adj, mt.GT, mt.age))
-
-    logger.info("Annotating with qual hists (on raw data)")
-    mt = mt.annotate_rows(**qual_hist_expr(mt.GT, mt.GQ, mt.DP, mt.AD))
-
-    logger.info("Annotating with qual hists (adj)")
-    mt_filt = filter_to_adj(mt)
-    mt_filt = mt_filt.select_rows()
-    mt_filt = mt_filt.annotate_rows(
-        **qual_hist_expr(mt_filt.GT, mt_filt.GQ, mt_filt.DP, mt_filt.AD)
-    )
-    ht = mt_filt.rows()
-    mt = mt.annotate_rows(adj_qual_hists=ht[mt.row_key])
-    return mt
-
-
-def calculate_inbreeding_coefficient(
-    mt: hl.MatrixTable, data_source: str, freeze: int, overwrite: bool
-) -> None:
-    """
-    Calculate inbreeding coefficient annotation and writes Table.
-
-    :param MatrixTable mt: Input MatrixTable with related samples removed.
-    :param str data_source: One of "regeneron" or "broad".
-    :param int freeze: One of the data freezes.
-    :param bool overwrite: Whether to overwrite data.
-    :return: None
-    :rtype: None
-    """
-    ht = mt.annotate_rows(InbreedingCoeff=bi_allelic_site_inbreeding_expr(mt.GT)).rows()
-    ht = ht.select("InbreedingCoeff")
-    ht.write(
-        var_annotations_ht_path("inbreeding_coefficient", data_source, freeze),
-        overwrite=overwrite,
-    )
-
-
 def generate_cohort_frequency_data(
-    mt: hl.MatrixTable, pops: List[str]
+    mt: hl.MatrixTable, pops: List[str], pop_col: str, sex_col,
 ) -> hl.MatrixTable:
     """
-    Generates frequency struct annotation containing AC, AF, AN, and homozygote count for all NFE individuals from input dataset.
-
-    Option to stratify by tranche as proxy for platform.
+    Generates frequency struct annotation containing AC, AF, AN, and homozygote count for all individuals of specified ancestry from input dataset.
 
     :param MatrixTable mt: Input MatrixTable annotated with sample metadata information.
     :param list pops: Populations to include in frequency calculation.
+    :param str pop_col: Name of column containing population information.
+    :param str sex_col: Name of column containing sex karyotype information.
     :return: MatrixTable with frequency annotations in struct named `cohort_freq` and metadata in globals named `freq_meta`.
     :rtype: hl.MatrixTable
     """
@@ -104,17 +49,15 @@ def generate_cohort_frequency_data(
         f"Filtering to {pops} to calculate cohort frequency (includes related samples)..."
     )
     pops = hl.literal(pops)
-    mt = mt.filter_cols(pops.contains(mt.meta.hybrid_pop_data.pop))
+    mt = mt.filter_cols(pops.contains(mt[pop_col]))
     mt = mt.filter_rows(hl.agg.any(mt.GT.is_non_ref()))
 
     logger.info("Generating frequency data...")
-    mt = annotate_freq(mt, sex_expr=mt.meta.sex_imputation.sex_karyotype,)
+    mt = annotate_freq(mt, sex_expr=mt[sex_col])
     mt = mt.transmute_rows(cohort_freq=mt.freq).select_rows("cohort_freq")
     cohort_freq_meta = hl.eval(mt.freq_meta)
     for i in range(len(cohort_freq_meta)):
-        if i == 0:
-            update_dict = {"group": "cohort_adj"}
-        elif i == 1:
+        if i == 1:
             update_dict = {"group": "cohort_raw"}
         else:
             update_dict = {"group": "cohort"}
@@ -130,12 +73,17 @@ def generate_frequency_data(
     pops_to_remove_for_popmax: List[str],
     cohort_frequency_pops: List[str],
     overwrite: bool,
-    calculate_by_tranche: bool,
+    platform_strata: bool = False,
+    tranche: bool = False,
+    platform_expr: Optional[hl.expr.StringExpression] = "meta.ukbb_meta.batch",
 ) -> hl.Table:
     """
     Generates frequency struct annotation containing AC, AF, AN, and homozygote count for input dataset stratified by population.
 
-    Option to stratify by tranche as proxy for platform.
+    Option to stratify by platform (either inferred platform or tranche as proxy for platform).
+
+    .. note::
+        This function expects dense format data.
 
     :param MatrixTable mt: Input MatrixTable.
     :param str data_source: One of "regeneron" or "broad".
@@ -143,24 +91,41 @@ def generate_frequency_data(
     :param list pops_to_remove_for_popmax: List of populations to exclude from popmax calculations. 
     :param list cohort_frequency_pops: List of populations to include in cohort frequency calculations.
     :param bool overwrite: Whether to overwrite data.
+    :param bool platform_strata: Whether to calculate frequencies per platform or tranche as proxy for platform.
+    :param bool tranche: Whether to use tranche (as a proxy for platform) instead of inferred platform.
+    :param hl.expr.StringExpression platform_expr: Platform or tranche StringExpression.
     :param bool calculate_by_tranche: Whether to calculate frequencies per tranche.
     :return: Table with frequency annotations in struct named `freq` and metadata in globals named `freq_meta`.
     :rtype: Table
     """
-    if calculate_by_tranche:
-        additional_strata_expr = {"tranche": mt.tranche}
+    if platform_strata:
+        if tranche:
+            name = "tranche"
+        else:
+            name = "platform"
+        additional_strata_expr = {f"{name}": platform_expr}
     else:
         additional_strata_expr = None
 
     logger.info("Generating cohort frequency data...")
-    mt = generate_cohort_frequency_data(mt, cohort_frequency_pops)
+    mt = generate_cohort_frequency_data(
+        mt,
+        cohort_frequency_pops,
+        "meta.hybrid_pop_data.pop",
+        "meta.sex_imputation.sex_karyotype",
+    )
 
     logger.info("Filtering related samples...")
     mt = mt.filter_cols(~mt.meta.sample_filters.related)
 
     logger.info("Calculating InbreedingCoefficient...")
     # NOTE: This is not the ideal location to calculate this, but added here to avoid another densify
-    calculate_inbreeding_coefficient(mt, data_source, freeze, overwrite)
+    ht = mt.annotate_rows(InbreedingCoeff=bi_allelic_site_inbreeding_expr(mt.GT)).rows()
+    ht = ht.select("InbreedingCoeff")
+    ht.write(
+        var_annotations_ht_path("inbreeding_coefficient", data_source, freeze),
+        overwrite=overwrite,
+    )
 
     logger.info("Generating frequency data...")
     mt = annotate_freq(
@@ -255,33 +220,22 @@ def main(args):
         f"Including {cohort_frequency_pops} in cohort frequency calculations"
     )
 
-    # Read in hardcalls and filter to high quality UKBB samples only
-    mt = get_ukbb_data(data_source, freeze, ukbb_samples_only=True, meta_root="meta")
-    mt = mt.filter_cols(mt.meta.sample_filters.high_quality)
+    # Read in hardcalls
+    mt = get_ukbb_data(data_source, freeze, meta_root="meta")
 
-    if not args.skip_densify:
-        logger.info("Reading in capture HT a")
+    if args.compute_frequency:
+        logger.info("Reading in capture HT")
         capture_ht = hl.read_table(capture_ht_path(data_source))
-
-        logger.info("Computing adj and sex adjusted genotypes")
-        mt = hl.experimental.sparse_split_multi(mt, filter_changed_loci=True)
-        mt = mt.select_entries(
-            GT=adjusted_sex_ploidy_expr(mt.locus, mt.GT, mt.meta.sex_karyotype),
-            adj=get_adj_expr(mt.GT, mt.GQ, mt.DP, mt.AD),
-        )
 
         logger.info("Densifying...")
         mt = hl.experimental.densify(mt)
         mt = mt.filter_rows(hl.len(mt.alleles) > 1)
         mt = mt.filter_rows(hl.is_defined(capture_ht[mt.locus]))
-        mt = mt.filter_rows(hl.agg.any(mt.GT.is_non_ref()))
 
-    if args.by_platform:
-        logger.info(
-            "Annotating sample with tranche information (from array sample map ht)"
-        )
-        sample_map_ht = hl.read_table(array_sample_map_ht_path(freeze))
-        mt = mt.annotate_cols(tranche=sample_map_ht[mt.col_key].batch)
+        # Filter to high quality UKBB samples only
+        mt = mt.filter_cols(mt.meta.sample_filters.high_quality)
+        mt = mt.filter_cols(hl.is_defined(mt.meta.ukbb_meta.batch))
+        mt = mt.filter_rows(hl.agg.any(mt.GT.is_non_ref()))
 
         logger.info("Calculating frequencies")
         ht = generate_frequency_data(
@@ -292,6 +246,7 @@ def main(args):
             cohort_frequency_pops,
             args.overwrite,
             args.by_platform,
+            args.by_tranche,
         )
 
         ht = ht.repartition(args.n_partitions)
@@ -334,17 +289,15 @@ if __name__ == "__main__":
         help="Pops to include in cohort frequency calculations",
     )
     parser.add_argument(
-        "--skip_densify", help="Skip densifying data", action="store_true",
+        "--compute_frequency", help="Compute frequency data data", action="store_true",
     )
     parser.add_argument(
-        "--calculate_frequencies",
-        help="Calculate most frequency data",
+        "--by_platform",
+        help="Calculate frequencies stratified by inferred platform",
         action="store_true",
     )
     parser.add_argument(
-        "--by_tranche",
-        help="Also calculate frequencies by UKBB data tranche",
-        action="store_true",
+        "--by_tranche", help="Use tranche as a proxy for platform", action="store_true",
     )
     parser.add_argument(
         "-j",
