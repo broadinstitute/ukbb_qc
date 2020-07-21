4a70f4917d964d933efc63a6d97ea18c3aab0620
Katherine Chao <kchao@broadinstitute.org>
2020-07-21 13:27:09 -0400

    Restructured release to only write release HT, also moved constants to common repo

--- ukbb_qc/release/prepare_internal_data_release.py
+++ ukbb_qc/release/prepare_internal_data_release.py
@@ -6,12 +6,18 @@ import hail as hl
 
 from gnomad.resources.grch38.reference_data import dbsnp, lcr_intervals
 from gnomad.utils.slack import slack_notifications
+from gnomad.utils.vcf import (
+    AS_FIELDS,
+    ENTRIES,
+    RF_FIELDS,
+    SITE_FIELDS,
+    SPARSE_ENTRIES,
+    VQSR_FIELDS,
+)
 from ukbb_qc.resources.basics import (
-    capture_ht_path,
     get_ukbb_data,
     logging_path,
     release_ht_path,
-    release_mt_path,
 )
 from ukbb_qc.resources.resource_utils import CURRENT_FREEZE
 from ukbb_qc.resources.variant_qc import info_ht_path, var_annotations_ht_path
@@ -25,52 +31,6 @@ logging.basicConfig(
 )
 logger = logging.getLogger("data_release")
 logger.setLevel(logging.INFO)
-
-AS_FIELDS = [
-    "AS_BaseQRankSum",
-    "AS_FS",
-    "AS_MQ",
-    "AS_MQRankSum",
-    "AS_pab_max",
-    "AS_QD",
-    "AS_ReadPosRankSum",
-    "AS_SOR",
-    "AS_VarDP",
-]
-"""
-Allele-specific variant annotations to be extracted from the info HT.
-"""
-SITE_FIELDS = [
-    "FS",
-    "InbreedingCoeff",
-    "MQ",
-    "MQRankSum",
-    "QD",
-    "ReadPosRankSum",
-    "sibling_singleton",
-    "SOR",
-    "transmitted_singleton",
-    "VarDP",
-]
-"""
-Site level variant annotations to be extracted from the info HT.
-"""
-RF_FIELDS = [
-    "interval_qc_pass",
-    "fail_hard_filters",
-    "filters",
-    "rf_label",
-    "rf_train",
-    "rf_probability",
-    "tp",
-]
-"""
-Annotations specific to the random forest model to be extracted from the random forest HT.
-"""
-VQSR_FIELDS = ["AS_VQSLOD", "AS_culprit", "NEGATIVE_TRAIN_SITE", "POSITIVE_TRAIN_SITE"]
-"""
-Annotations specific to VQSR to be extracted from the VQSR HT.
-"""
 
 
 def flag_problematic_regions(
@@ -90,12 +50,11 @@ def flag_problematic_regions(
     :rtype: hl.expr.StructExpression
     """
     lcr_ht = lcr_intervals.ht()
-    capture_ht = hl.read_table(capture_ht_path("broad"))
 
     return hl.struct(
         lcr=hl.is_defined(lcr_ht[t.locus]),
         fail_interval_qc=~(t.rf.interval_qc_pass),
-        in_capture_region=hl.is_defined(capture_ht[t.locus]),
+        in_capture_region=tf.rf.in_capture_interval,
     )
 
 
@@ -127,7 +86,6 @@ def prepare_annotations(
         (hl.agg.any(mt.GT.is_non_ref() | hl.is_defined(mt.END)))
         & (~info_ht[mt.row_key].AS_lowqual)
     )
-    logger.info(f"Count after filtering out low QUAL variants {mt.count()}")
 
     logger.info("Selecting annotations from annotation Tables...")
     info_fields = SITE_FIELDS + AS_FIELDS
@@ -150,11 +108,9 @@ def prepare_annotations(
     )  # NOTE: Will need to drop InbreedingCoeff annotation here in 500K, but this isn't present in 300K
     dbsnp_ht = dbsnp.ht().select("rsid")
 
-    logger.info(f"Frequency HT before filtering out chrM: {freq_ht.count()}")
     freq_ht = hl.filter_intervals(
         freq_ht, [hl.parse_locus_interval("chrM")], keep=False
     )
-    logger.info(f"Frequency HT count after filtering out chrM: {freq_ht.count()}")
 
     logger.info(
         "Annotating MT with random forest HT and flagging problematic regions..."
@@ -176,7 +132,7 @@ def prepare_annotations(
         rsid=dbsnp_ht[mt.row_key].rsid,
         qual=info_ht[mt.row_key].qual,
     )
-    mt = mt.annotate_globals(rf=rf_ht.index_globals())
+    mt = mt.annotate_globals(rf_globals=rf_ht.index_globals())
     logger.info(f"Final MT count: {mt.count()}")
     return mt
 
@@ -188,70 +144,45 @@ def main(args):
     freeze = args.freeze
 
     try:
-        if args.prepare_internal_mt:
-            logger.info("Getting raw MT and dropping all unnecessary entries...")
-            # NOTE: reading in raw MatrixTable to be able to return all samples/variants
-            mt = get_ukbb_data(
-                data_source,
-                freeze,
-                key_by_locus_and_alleles=args.key_by_locus_and_alleles,
-                split=False,
-                raw=True,
-                repartition=args.repartition,
-                n_partitions=args.raw_partitions,
-            ).select_entries(
-                "DP",
-                "GQ",
-                "LA",
-                "LAD",
-                "LGT",
-                "LPGT",
-                "LPL",
-                "MIN_DP",
-                "PID",
-                "RGQ",
-                "SB",
-            )
+        logger.info("Getting raw MT and dropping all unnecessary entries...")
+        # NOTE: reading in raw MatrixTable to be able to return all samples/variants
+        mt = get_ukbb_data(
+            data_source,
+            freeze,
+            key_by_locus_and_alleles=args.key_by_locus_and_alleles,
+            split=False,
+            raw=True,
+            repartition=args.repartition,
+            n_partitions=args.raw_partitions,
+        ).select_entries(*SPARSE_ENTRIES)
 
-            logger.info("Splitting raw MT...")
-            mt = hl.experimental.sparse_split_multi(mt)
-            mt = mt.select_entries(
-                "GT", "GQ", "DP", "AD", "MIN_DP", "PGT", "PID", "PL", "SB"
-            )
+        logger.info("Splitting raw MT...")
+        mt = hl.experimental.sparse_split_multi(mt)
+        mt = mt.select_entries(*ENTRIES)
 
-            logger.info("Reading in all variant annotation tables...")
-            info_ht = hl.read_table(info_ht_path(data_source, freeze))
-            freq_ht = hl.read_table(
-                "join_freq", var_annotations_ht_path(data_source, freeze)
-            )
-            rf_ht = hl.read_table(var_annotations_ht_path("rf", data_source, freeze))
-            vep_ht = hl.read_table(var_annotations_ht_path("vep", data_source, freeze))
-            allele_ht = hl.read_table(
-                var_annotations_ht_path("allele_data", data_source, freeze)
-            )
-            vqsr_ht = hl.read_table(
-                var_annotations_ht_path("vqsr", data_source, freeze)
-            )
+        logger.info("Reading in all variant annotation tables...")
+        info_ht = hl.read_table(info_ht_path(data_source, freeze))
+        freq_ht = hl.read_table(
+            "join_freq", var_annotations_ht_path(data_source, freeze)
+        )
+        rf_ht = hl.read_table(var_annotations_ht_path("rf", data_source, freeze))
+        vep_ht = hl.read_table(var_annotations_ht_path("vep", data_source, freeze))
+        allele_ht = hl.read_table(
+            var_annotations_ht_path("allele_data", data_source, freeze)
+        )
+        vqsr_ht = hl.read_table(var_annotations_ht_path("vqsr", data_source, freeze))
 
-            logger.info("Adding annotations...")
-            mt = prepare_annotations(
-                mt, info_ht, freq_ht, rf_ht, vep_ht, allele_ht, vqsr_ht,
-            )
+        logger.info("Adding annotations...")
+        mt = prepare_annotations(
+            mt, info_ht, freq_ht, rf_ht, vep_ht, allele_ht, vqsr_ht,
+        )
 
-            logger.info("Getting age hist data...")
-            age_hist_data = get_age_distributions(get_age_ht(freeze))
-            mt = mt.annotate_globals(age_distribution=age_hist_data)
-            mt.write(release_mt_path(data_source, freeze), args.overwrite)
-
-        if args.prepare_browser_ht:
-            mt = hl.read_matrix_table(release_mt_path(data_source, freeze))
-            logger.info(f"Release MT count: {mt.count()}")
-
-            ht = mt.rows()
-            logger.info(f"Release HT count: {ht.count()}")
-
-            ht = ht.naive_coalesce(args.n_partitions)
-            ht.write(release_ht_path(data_source, freeze), args.overwrite)
+        logger.info("Getting age hist data...")
+        age_hist_data = get_age_distributions(get_age_ht(freeze))
+        mt = mt.annotate_globals(age_distribution=age_hist_data)
+        ht = mt.rows()
+        ht = ht.naive_coalesce(args.n_partitions)
+        ht.write(release_ht_path(data_source, freeze), args.overwrite)
 
     finally:
         logger.info("Copying hail log to logging bucket...")
@@ -286,14 +217,6 @@ if __name__ == "__main__":
         help="Number of desired partitions for output Tables",
         default=10000,
         type=int,
-    )
-    parser.add_argument(
-        "--prepare_internal_mt",
-        help="Prepare internal MatrixTable",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--prepare_browser_ht", help="Prepare sites HT for browser", action="store_true"
     )
 
     parser.add_argument(
