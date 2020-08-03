e35f540ea2f577d35cd83d931de9ea67886a016a
Katherine Chao <kchao@broadinstitute.org>
2020-08-03 14:35:04 -0400

    Added note about segdup/decoy in filters_sanity_check

--- ukbb_qc/assessment/sanity_checks.py
+++ ukbb_qc/assessment/sanity_checks.py
@@ -135,6 +135,7 @@ def filters_sanity_check(ht: hl.Table) -> None:
     logger.info(
         f"hl.agg.counter filters: {ht_explode.aggregate(hl.agg.counter(ht_explode.filters))}"
     )
+    # NOTE: in_problematic_region check will need to be updated if we get hg38 decoy and segdup files
     ht = ht.annotate(
         is_filtered=hl.len(ht.filters) > 0,
         in_problematic_region=hl.any(
