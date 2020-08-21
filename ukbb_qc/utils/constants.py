2ce19632a6910e6cb159899afa464c31ae8ba480
Katherine Chao <kchao@broadinstitute.org>
2020-08-21 09:56:36 -0400

    Fixed some small errors made during rebase

--- ukbb_qc/utils/constants.py
+++ ukbb_qc/utils/constants.py
@@ -9,21 +9,3 @@ Sample sexes used in VCF export.
 
 Used to stratify frequency annotations (AC, AN, AF) for each sex in UKBB.
 """
-
-ANNOTATIONS_HISTS = {
-    "AS_FS": (0, 50, 50),
-    "InbreedingCoeff": (-0.25, 0.25, 50),
-    "AS_MQ": (0, 80, 40),
-    "AS_MQRankSum": (-15, 15, 60),
-    "AS_QD": (0, 40, 40),
-    "AS_ReadPosRankSum": (-15, 15, 60),
-    "AS_SOR": (0, 10, 50),
-    "AS_BaseQRankSum": (-15, 15, 60),
-    "AS_VarDP": (1, 9, 32),
-    "AS_VQSLOD": (-30, 30, 60),
-    "rf_tp_probability": (0, 1, 50),
-    "AS_pab_max": (0, 1, 50),
-}
-"""
-Dictionary of metric names and their histogram values (start, end, bins).
-"""
