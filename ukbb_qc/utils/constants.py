548a9119c4941eb8817c20d9b4a1ed8782ec779d
Katherine Chao <kchao@broadinstitute.org>
2021-03-05 10:28:30 -0500

    renamed ukbb_sexes in constants

--- ukbb_qc/utils/constants.py
+++ ukbb_qc/utils/constants.py
@@ -3,11 +3,13 @@ INTERVAL_QC_PARAMETERS = (85, 20)
 Parameters used for interval QC (sample percentage cutoff, coverage cutoff).
 """
 
-SEXES_UKBB = ["XX", "XY"]
+UKBB_SEXES = ["XX", "XY"]
 """
 Sample sexes used in VCF export.
 
 Used to stratify frequency annotations (AC, AN, AF) for each sex in UKBB.
+No longer necessary -- this constant also exists in gnomAD repo now:
+https://github.com/broadinstitute/gnomad_methods/blob/master/gnomad/resources/grch38/gnomad.py#L29
 """
 
 UKBB_POPS = {
