2016554cfd6a87d4c6c9da2aac07bdb7fb6a20b3
Katherine Chao <kchao@broadinstitute.org>
2021-07-08 12:01:00 -0400

    Addressed review comments

--- ukbb_qc/readviz/generate_tsv_of_cram_paths.py
+++ ukbb_qc/readviz/generate_tsv_of_cram_paths.py
@@ -4,12 +4,15 @@ import logging
 import hail as hl
 
 from gnomad.resources.resource_utils import DataException
+from gnomad.utils.file_utils import file_exists
 from gnomad.utils.slack import slack_notifications
 
+
+from ukbb_qc.resources.utils.utils import get_sample_ids
 from ukbb_qc.resources.basics import (
     cram_map_path,
     readviz_haplotype_caller_path,
-    readviz_ht_exploded_path,
+    readviz_ids_tsv_path,
     readviz_per_sample_tsv_path,
 )
 from ukbb_qc.resources.resource_utils import CURRENT_FREEZE
@@ -22,33 +25,28 @@ logger.setLevel(logging.INFO)
 
 
 def main(args):
-    data_source = "broad"
     freeze = args.freeze
-    tranche_data = (data_source, freeze)
 
-    ht = hl.read_table(readviz_ht_exploded_path()).select()
-    ht = ht.annotate(
-        variants_tsv_bgz=hl.format("%s/%s.tsv.bgz", readviz_per_sample_tsv_path(), ht.S)
-    )
+    sample_ids = get_sample_ids(readviz_ids_tsv_path(freeze))
+    with hl.hadoop_open(cram_map_path(freeze)) as c, hl.hadoop_open(
+        f"{readviz_haplotype_caller_path()}/inputs/step4_output_cram_and_tsv_paths_table.tsv",
+        "w",
+    ) as o:
+        o.write("sample_id\tcram\tcrai\tvariants_tsv_bgz\n")
 
-    cram_ht = hl.import_table(cram_map_path(*tranche_data), no_header=True).key_by("f0")
-    cram_ht = cram_ht.annotate(crai_path=hl.format("%s.crai", cram_ht.f1))
-    cram_ht = cram_ht.rename({"f0": "S", "f1": "cram_path"})
-
-    ht = ht.annotate(**cram_ht[ht.key])
-    if ht.aggregate(hl.agg.count_where(hl.is_missing(ht.cram_path) > 0)):
-        raise DataException(
-            "Some samples are missing a cram path. Please double check and restart!"
-        )
-    if ht.aggregate(hl.agg.count_where(hl.is_missing(ht.variants_tsv_bgz) > 0)):
-        raise DataException(
-            "Some samples are missing their variants TSV files. Please double check and restart!"
-        )
-
-    ht = ht.rename({"S": "sample_id"})
-    ht.export(
-        f"{readviz_haplotype_caller_path()}/inputs/step4_output_cram_and_tsv_paths_table.tsv"
-    )
+        for line in c:
+            sample, cram = line.strip().split("\t")
+            if sample in sample_ids:
+                tsv = f"{readviz_per_sample_tsv_path()}/{sample}.tsv.bgz"
+                if not file_exists(cram):
+                    raise DataException(
+                        f"{sample} is missing a cram path. Please double check and restart!"
+                    )
+                if not file_exists(tsv):
+                    raise DataException(
+                        f"{sample} is missing their variants TSV file. Please double check and restart!"
+                    )
+                o.write(f"{sample}\t{cram}\t{cram}.crai\t{tsv}\n")
 
 
 if __name__ == "__main__":
