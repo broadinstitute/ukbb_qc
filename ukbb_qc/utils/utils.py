7a02efdbc43a90ac2a6f754a202f648c5dd1d819
Katherine Chao <kchao@broadinstitute.org>
2021-07-08 11:54:51 -0400

    Moved get_sample_ids to utils

--- ukbb_qc/utils/utils.py
+++ ukbb_qc/utils/utils.py
@@ -99,6 +99,24 @@ def join_tables(
 
 
 # Sample resources
+def get_sample_ids(ids_file: str, header: bool = False) -> List[str]:
+    """
+    Open file with all sample IDs and stores IDs in a list.
+
+    :param str ids_file: Path to file containing sample IDs with variants to extract for readviz.
+    :param bool header: Whether IDs file has a header line. Default is False.
+    :return: List of sample IDs
+    :rtype: List[str]
+    """
+    sample_ids = []
+    with open(ids_file) as i:
+        if header:
+            header = i.readline()
+        for line in i:
+            sample_ids.append(line.strip())
+    return sample_ids
+
+
 def remove_hard_filter_samples(
     data_source: str,
     freeze: int,
