3e232dc803295b1a3ffddadbea9959647a1312d5
Katherine Chao <kchao@broadinstitute.org>
2021-09-28 14:09:13 -0400

    Update ukbb_qc/release/sample_data_dicts.py
    Co-authored-by: jkgoodrich <33063077+jkgoodrich@users.noreply.github.com>

--- ukbb_qc/release/sample_data_dicts.py
+++ ukbb_qc/release/sample_data_dicts.py
@@ -192,7 +192,7 @@ SAMPLE_ANNOTATIONS = {
                 )
             },
             "fail_n_snp": {
-                "Description": "Whether the sample was removed from QC for having a n_snp value larger upper MAD or smaller than the lower MAD threshold."
+                "Description": "Whether the sample was removed from QC for having a n_snp value larger than the upper MAD or smaller than the lower MAD threshold."
             },
             "fail_r_ti_tv": {
                 "Description": "Whether the sample was removed from QC for having a r_ti_tv value larger upper MAD or smaller than the lower MAD threshold."
