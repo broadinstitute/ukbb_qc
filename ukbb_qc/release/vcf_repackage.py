7a16ad1778267812e2f3c21faaad36af37ebc57d
Katherine Chao <kchao@broadinstitute.org>
2021-09-28 15:55:23 -0400

    updated batch code to run once per contig

--- ukbb_qc/release/vcf_repackage.py
+++ ukbb_qc/release/vcf_repackage.py
@@ -10,6 +10,8 @@ from tgg.batch.batch_utils import (
     run_batch,
 )
 
+from gnomad.utils.reference_genome import get_reference_genome
+
 from ukbb_qc.resources.basics import release_vcf_path
 from ukbb_qc.resources.resource_utils import CURRENT_FREEZE
 
@@ -45,38 +47,47 @@ def main(args):
     output_dir = args.output_dir
 
     logger.info("Getting VCF shards...")
-    path = f"{release_vcf_path(*tranche_data, contig=None)}/*bgz"
-    shards = (
-        subprocess.check_output(["gsutil", "ls", path])
-        .decode("utf8")
-        .strip()
-        .split("\n")
-    )
+    rg = get_reference_genome("GRCh38")
+    contigs = rg.contigs[:24]  # autosomes + X/Y
+    logger.info(f"Contigs: {contigs}")
 
-    # Process shards
-    with run_batch(args, batch_name="Repackage VCF shards") as batch:
-        for shard in tqdm(shards, unit="shards"):
-            # Naming the output shard with 'repackaged' + gz extension since that is the extension DNANexus uses in their code
-            output_name = f"{os.path.split(shard)[-1].split('.')[0]}.repackaged.gz"
-            output_tabix = f"{output_name}.tbi"
-            j = init_job(
-                batch, f"{shard} repackage", DOCKER_IMAGE, args.cpu, args.memory,
-            )
-            j.command(
-                f"""gcloud -q auth activate-service-account --key-file=/gsa-key/key.json"""
-            )
-            local_vcf_path = localize_file(j, shard, use_gcsfuse=True)
-            ukbb_header_reformat_sh_contents = open("ukbb_header_reformat.sh").read()
-            j.command(
-                f"""cat <<"EOF" > ukbb_header_reformat.sh
-{ukbb_header_reformat_sh_contents} 
-EOF"""
-            )
-            j.command(
-                f"""bash ukbb_header_reformat.sh {local_vcf_path} {output_name}"""
-            )
-            j.command(f"""gsutil -m cp {output_name} {output_dir}""")
-            j.command(f"""gsutil -m cp {output_tabix} {output_dir}""")
+    for contig in contigs:
+        path = f"{release_vcf_path(*tranche_data, contig=contig)}/*bgz"
+        shards = (
+            subprocess.check_output(["gsutil", "ls", path])
+            .decode("utf8")
+            .strip()
+            .split("\n")
+        )
+
+        # Process shards
+        with run_batch(args, batch_name="Repackage VCF shards") as batch:
+            for shard in tqdm(shards, unit="shards"):
+                # Naming the output shard with this format:
+                # ukb<field_id>_chr<#>_block<#>.repackaged.gz
+                # Using 'repackaged' + gz extension since that is the extension DNANexus uses in their code
+                output_name = f"ukb26041_{contig}_block{os.path.split(shard)[-1].split('.')[0].split('-')[1]}.repackaged.gz"
+                output_tabix = f"{output_name}.tbi"
+                j = init_job(
+                    batch, f"{shard} repackage", DOCKER_IMAGE, args.cpu, args.memory,
+                )
+                j.command(
+                    f"""gcloud -q auth activate-service-account --key-file=/gsa-key/key.json"""
+                )
+                local_vcf_path = localize_file(j, shard, use_gcsfuse=True)
+                ukbb_header_reformat_sh_contents = open(
+                    "ukbb_header_reformat.sh"
+                ).read()
+                j.command(
+                    f"""cat <<"EOF" > ukbb_header_reformat.sh
+    {ukbb_header_reformat_sh_contents} 
+    EOF"""
+                )
+                j.command(
+                    f"""bash ukbb_header_reformat.sh {local_vcf_path} {output_name}"""
+                )
+                j.command(f"""gsutil -m cp {output_name} {output_dir}""")
+                j.command(f"""gsutil -m cp {output_tabix} {output_dir}""")
 
 
 if __name__ == "__main__":
