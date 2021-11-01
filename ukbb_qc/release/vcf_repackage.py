b0e7a6fb01b65c6de155362c8551a0416347fd29
Katherine Chao <kchao@broadinstitute.org>
2021-11-01 09:36:20 -0400

    Changed vcf repackage to run one chrom at a time

--- ukbb_qc/release/vcf_repackage.py
+++ ukbb_qc/release/vcf_repackage.py
@@ -10,6 +10,7 @@ from tgg.batch.batch_utils import (
     run_batch,
 )
 
+from gnomad.resources.resource_utils import DataException
 from gnomad.utils.reference_genome import get_reference_genome
 
 from ukbb_qc.resources.basics import release_vcf_path
@@ -52,55 +53,57 @@ def main(args):
     contigs = rg.contigs[:24]  # autosomes + X/Y
     logger.info(f"Contigs: {contigs}")
 
-    for contig in contigs:
-        path = f"{release_vcf_path(*tranche_data, contig=contig)}/*bgz"
-        shards = (
-            subprocess.check_output(["gsutil", "ls", path])
-            .decode("utf8")
-            .strip()
-            .split("\n")
+    contig = args.contigs
+    if contig not in contigs:
+        raise DataException(
+            f"{contig} not in {contigs}. Please double check and restart!"
         )
 
-        # Process shards
-        with run_batch(args, batch_name="Repackage VCF shards") as batch:
-            for shard in tqdm(shards, unit="shards"):
-                # Naming the output shard with this format:
-                # ukb<field_id>_chr<#>_block<#>.repackaged.gz
-                # Using 'repackaged' + gz extension since that is the extension DNANexus uses in their code
-                output_name = f"ukb26041_{contig}_block{os.path.split(shard)[-1].split('.')[0].split('-')[1]}.repackaged.gz"
-                output_tabix = f"{output_name}.tbi"
-                output_coordinates = f"{output_name}_coordinates.tsv"
-                j = init_job(
-                    batch, f"{shard} repackage", DOCKER_IMAGE, args.cpu, args.memory,
-                )
-                j.command(
-                    f"""gcloud -q auth activate-service-account --key-file=/gsa-key/key.json"""
-                )
-                local_vcf_path = localize_file(j, shard, use_gcsfuse=True)
-                ukbb_header_reformat_sh_contents = open(
-                    "ukbb_header_reformat.sh"
-                ).read()
-                j.command(
-                    f"""cat <<"EOF" > ukbb_header_reformat.sh
-    {ukbb_header_reformat_sh_contents} 
-    EOF"""
-                )
-                j.command(
-                    f"""bash ukbb_header_reformat.sh {local_vcf_path} {output_name} > {output_coordinates}"""
-                )
-                j.command(f"""gsutil -u maclab-ukbb -m cp {output_name} {output_dir}""")
-                j.command(
-                    f"""gsutil -u maclab-ukbb -m cp {output_tabix} {output_dir}"""
-                )
-                j.command(
-                    f"""gsutil -m cp {output_coordinates} {coordinates_output_dir}"""
-                )
+    path = f"{release_vcf_path(*tranche_data, contig=contig)}/*bgz"
+    shards = (
+        subprocess.check_output(["gsutil", "ls", path])
+        .decode("utf8")
+        .strip()
+        .split("\n")
+    )
+
+    # Process shards
+    with run_batch(args, batch_name="Repackage VCF shards") as batch:
+        for shard in tqdm(shards, unit="shards"):
+            # Naming the output shard with this format:
+            # ukb<field_id>_chr<#>_block<#>.repackaged.gz
+            # Using 'repackaged' + gz extension since that is the extension DNANexus uses in their code
+            output_name = f"ukb26041_{contig}_block{os.path.split(shard)[-1].split('.')[0].split('-')[1]}.repackaged.gz"
+            output_tabix = f"{output_name}.tbi"
+            output_coordinates = f"{output_name}_coordinates.tsv"
+            j = init_job(
+                batch, f"{shard} repackage", DOCKER_IMAGE, args.cpu, args.memory,
+            )
+            j.command(
+                f"""gcloud -q auth activate-service-account --key-file=/gsa-key/key.json"""
+            )
+            local_vcf_path = localize_file(j, shard, use_gcsfuse=True)
+            ukbb_header_reformat_sh_contents = open("ukbb_header_reformat.sh").read()
+            j.command(
+                f"""cat <<"EOF" > ukbb_header_reformat.sh
+{ukbb_header_reformat_sh_contents} 
+EOF"""
+            )
+            j.command(
+                f"""bash ukbb_header_reformat.sh {local_vcf_path} {output_name} > {output_coordinates}"""
+            )
+            j.command(f"""gsutil -u maclab-ukbb -m cp {output_name} {output_dir}""")
+            j.command(f"""gsutil -u maclab-ukbb -m cp {output_tabix} {output_dir}""")
+            j.command(f"""gsutil -m cp {output_coordinates} {coordinates_output_dir}""")
 
 
 if __name__ == "__main__":
     p = init_arg_parser(default_cpu=8, default_billing_project="gnomad-production")
     p.add_argument(
         "--freeze", help="Data freeze to use", default=CURRENT_FREEZE, type=int
+    )
+    p.add_argument(
+        "--contig", help="Chromosome to repackage",
     )
     p.add_argument(
         "--output-dir",
